228ee3283f4f41fb75ea963c7e92da16609f8494
will_cr <christian.willberg@dlr.de>
2023-06-24 19:16:42 +0200

    new support structure

---
+++ src/Data/data_field_handling.jl
@@ -0,0 +1,14 @@
+include("../MPI_communication/MPI_send.jl")
+include("../MPI_communication/MPI_receive.jl")
+
+function field_exists()
+
+end
+
+function create_field()
+
+end
+
+function data_synchronisation()
+
+end
