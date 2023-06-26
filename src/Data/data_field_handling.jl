6d4e54189c9e7e30954f10f6bc66c13432f62675
will_cr <christian.willberg@dlr.de>
2023-06-26 12:33:27 +0200

    function set_basis_length_of_vectors(comm, master, globalLen)

--- src/Data/data_field_handling.jl
+++ src/Data/data_field_handling.jl
@@ -1,5 +1,20 @@
 include("../MPI_communication/MPI_send.jl")
+include("../MPI_communication/MPI_send.jl")
 include("../MPI_communication/MPI_receive.jl")
+
+
+# global nnodes = set_basis_length_of_vectors(comm, 0, lechunks)
+# is set in the input reader
+
+function set_basis_length_of_vectors(comm, master, globalLen)
+    msg = send_single_value_from_vector(comm, master, globalLen, Int32)
+    return msg[1]
+end
+
+function init_data_field(dof, type)
+    local_data = zeros(type, nnodes * dof, 1)
+    return local_data
+end
 
 function field_exists(name::String)
 
@@ -24,3 +39,4 @@ end
 function set_id(name::String)
 
 end
+
