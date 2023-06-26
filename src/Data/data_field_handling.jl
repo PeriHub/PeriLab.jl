43989dbcd65a20bac2d6bf7e98ed1c2389bf43b0
will_cr <christian.willberg@dlr.de>
2023-06-26 09:30:49 +0200

    topo bug fix

--- src/Data/data_field_handling.jl
+++ src/Data/data_field_handling.jl
@@ -1,14 +1,26 @@
 include("../MPI_communication/MPI_send.jl")
 include("../MPI_communication/MPI_receive.jl")
 
-function field_exists()
+function field_exists(name::String)
 
 end
 
-function create_field()
-
+function create_field(name::String, type, len::Int)
+    if field_exists(name)
+        return id = get_id(name)
+    else
+        id = set_id(name)
+    end
 end
 
 function data_synchronisation()
+
+end
+
+function get_id(name::String)
+
+end
+
+function set_id(name::String)
 
 end
