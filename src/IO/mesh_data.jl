d6cb2e26b7aff37a3a59c9a11e0286cc53722b01
will_cr <christian.willberg@dlr.de>
2023-06-30 06:20:43 +0200

    overlap prototype

--- src/IO/mesh_data.jl
+++ src/IO/mesh_data.jl
@@ -26,7 +26,11 @@ function load_mesh_and_distribute(params, comm)
         ranksize = MPI.Comm_size(comm)
         println("$(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
 
-        topo = distribute(meshdata, params, ranksize)
+        topo, ptc = distribute(meshdata, params, ranksize)
+        #topo, ptc = distribute(meshdata, params, 2)
+        overlap_map = create_overlap_map(topo, ptc, ranksize)
+        # overlap_map = create_overlap_map(topo, ptc, 2)
+        println(overlap_map)
     else
         #MPI.Barrier(comm) # notwendig?
     end
@@ -53,37 +57,82 @@ end
 function create_topology(nlist, size)
 
     nnodes = length(nlist)
-    size = 2
+
     if size == 1
         distribution = [collect(1:nnodes)]
+        overlap_map = [[[]]]
     else
-        distribution = create_base_chunk(nnodes, size)
+        distribution, ptc = create_base_chunk(nnodes, size)
         println("distrib $distribution")
         println()
         # check neighborhood & overlap -> all nodes after chunk are overlap
         for i in 1:size
             nchunks = length(distribution[i])
+            tempid = Int64[]
             for j in 1:nchunks
-                id = distribution[i, j]
-                nneighbors = length(nlist[id])
-                for k in 1:nneighbors
-                    if length(filter(x -> x == neighbors[nlist[id, k]], distribution[i])) < 1
-                        append!(distribution[i], [nlist[id, k]])
-                    end
+                id = distribution[i][j]
+                # find all nodes which are not in chunk
+                # add them to list as slave nodes for data exchange
+                # findall give the index of the valid statement
+                # that means that this indices have to be used to obtain the values
+                indices = findall(item -> item != i, ptc[nlist[id]])
+                if length(indices) > 0
+                    append!(tempid, nlist[id][indices])
                 end
             end
+            # only single new elements where added
+            append!(distribution[i], sort(unique(tempid)))
         end
 
     end
     println("distrib $distribution")
-    return distribution
+    return distribution, ptc
+end
+function create_overlap_map(distribution, ptc, size)
+    #[[[], [[2], [1]], [[2], [5]]], [[[1], [3]], [], [[1, 2], [6, 7]]], [[], [[1, 2], [4, 5]], []]]
+    overlap_map = _init_overlap_map_(size)
+    for i in 1:size
+        vector = distribution[i]
+        indices = findall(item -> item != i, ptc[distribution[i]])
+        le = length(indices)
+        from_index = []
+        to_index = []
+        for j in 1:le
+            from_index = findfirst(item -> item == vector[indices[j]], distribution[ptc[vector[indices[j]]]])
+            to_index = indices[j]
+            append!(overlap_map[ptc[vector[indices[j]]]][i][1], from_index)
+            append!(overlap_map[ptc[vector[indices[j]]]][i][2], to_index)
+        end
+
+    end
+    return overlap_map
 end
 
+
+function _init_overlap_map_(size)
+    #[[[], [[2], [1]], [[2], [5]]], [[[1], [3]], [], [[1, 2], [6, 7]]], [[], [[1, 2], [4, 5]], []]]
+    overlap_map = []
+    for i in 1:size
+        append!(overlap_map, [[]])
+        for j in 1:size
+            append!(overlap_map[i], [[]])
+        end
+    end
+    for i in 1:size
+        for j in 1:size
+            append!(overlap_map[i][j], [Int64[], Int64[]])
+        end
+    end
+    return overlap_map
+end
 function create_base_chunk(nnodes, size)
     # Calculate the initial size of each chunk
+    # for a nearly equal number of nodes vs. cores this algorithm might lead to the problem, 
+    # that the last core is not equally loaded
     chunk_size = div(nnodes, size)
     # Split the data into chunks
     distribution = fill([], size)
+    point_to_core = zeros(Int32, nnodes)
     for i in 1:size
         start_idx = (i - 1) * chunk_size + 1
         end_idx = min(i * chunk_size, nnodes)
@@ -91,8 +140,9 @@ function create_base_chunk(nnodes, size)
             end_idx = nnodes
         end
         distribution[i] = collect(start_idx:end_idx)
+        point_to_core[start_idx:end_idx] .= i
     end
-    return distribution
+    return distribution, point_to_core
 end
 
 function neighbors(mesh, params, coor)
