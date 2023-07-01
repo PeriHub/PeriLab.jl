6e7d0f7367a622cf6351b9917f1c71c39b3107d5
will_cr <christian.willberg@dlr.de>
2023-07-01 06:49:45 +0200

    helpers

---
+++ src/Support/Parameters/helpers.jl
@@ -0,0 +1,8 @@
+function dof_index(index, inds)
+    dest = Vector{eltype(index)}(undef, inds * length(index))
+    i = 1
+    @inbounds for a in index, b in 1:inds
+        dest[i+=1] = a * inds + b - inds
+    end
+    dest
+end
