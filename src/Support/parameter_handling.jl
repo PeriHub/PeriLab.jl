7ec059cd39df006b9bd396fd71a7f17fb70fba05
will_cr <christian.willberg@dlr.de>
2023-07-01 06:50:02 +0200

    fieldmanager

--- src/Support/parameter_handling.jl
+++ src/Support/parameter_handling.jl
@@ -3,3 +3,65 @@ include("./Parameters/parameter_handling_blocks.jl")
 function check_element(data, key)
     return haskey(data, key)
 end
+
+global fieldnames = Dict(Int64 => Dict(),Float32=>Dict(), Bool=>Dict())
+global nnodes::Int64 = 0 # where to put?
+global nbonds::Int64 = 0 # where to put?
+
+function create_node_field(name::String, type::DataType, dof::Int64)
+    return create_field(name, type,"Node", dof, true)
+end
+function create_constant_node_field(name::String, type::DataType, dof::Int64)
+    return create_field(name, type,"Node", dof, false)
+end
+function create_bond_field(name::String, type::DataType,dof::Int64)
+    return create_field(name, type,"Bond", dof, true)
+end
+function create_constant_bond_field(name::String, type::DataType, dof::Int64)
+    return create_field(name, type,"Bond", dof, false)
+end
+# wenn nicht existiert, wird an den gesamtvector der Teil angehängt. Dann wird der Vectorabschnitt zurückgeschickt und ist in der jeweiligen routine nutzbar
+# bisher wird alles synchrononisiert
+# nicht synchronisierte vectoren brauchen nicht hier rein -> Problem ist, wie mit der synch Option zu verfahren ist (eine stelle synch andere non synch)Was zählt?
+function create_field(name::String, type::DataType, bondNode::String, dof::Int64, time_dependend::bool)
+
+    if !haskey(fieldnames, type)
+        fieldnames[type] = Dict()
+    end
+    if haskey(fieldnames[type], name)
+        return fieldnames[type][name]
+    else
+        len = field_length(bondNode,time_dependend) * dof
+
+        if length(values(fieldnames[type])) == 0
+            num = 0
+        else
+            # start ende -> alles in einen Vector, dann sind weniger synchros nötig
+            num = maximum(values(fieldnames[type]))[2]
+            fieldnames[type][name] = (num + 1,num + len +1 )
+        end
+        return fieldnames[type][name]
+    end
+end
+
+function field_length(bondNode::String,time_dependend::bool)
+    le = 0
+    if bondNode=="Bond"
+        le = nbonds
+    elseif bondNode=="Node"
+        le = nnodes
+    else
+        le = 0
+        @error "Not supported option $bondNode allowed options are Bond::String or Node::String"
+    end
+    if time_dependend
+        le *= 2
+    end
+    return le
+end
+function key_magic(name::String, type, bondNode::String, dof::Int64, time_dependend::bool)
+
+#integer liste wo die keys geführt werden -> nötig?
+end
+
+function get_field(name::String)
