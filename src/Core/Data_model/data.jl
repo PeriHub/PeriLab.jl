13cf4f8f70df72b182586a946c9ad2bfeb50ec9b
will_cr <christian.willberg@dlr.de>
2023-07-02 18:00:22 +0200

    starting datamodel

--- src/Core/Data_model/data.jl
+++ src/Core/Data_model/data.jl
@@ -1,9 +1,23 @@
 include("../../Support/parameter_handling.jl")
+module DataModel
 
+data_model = Any
 
 function init_global_data(data)
 
     global n_blocks = get_number_of_blocks(data)
 
     #global nnodes = 
+end
+
+function add_field()
+
+end
+
+export init_data
+export add_data
+export data_model
+
+
+
 end
