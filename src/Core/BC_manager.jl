a1be284048669d8460897955802247d5ca2b2965
will_cr <christian.willberg@dlr.de>
2023-07-03 09:40:28 +0200

    bc manager module

---
+++ src/Core/BC_manager.jl
@@ -0,0 +1,18 @@
+module Boundary_conditions
+function interpret_BC(params, data)
+    # check bc
+end
+
+
+function init_BCs()
+
+end
+
+function time_depended_bc()
+
+end
+
+export init_BCs()
+export time_depended_bc()
+
+end
