32a0ffd810bc28dfdb795ead141ff63b9ed96b3c
will_cr <christian.willberg@dlr.de>
2023-07-07 14:27:55 +0200

    renaming

--- src/Support/data_manager.jl
+++ src/Support/data_manager.jl
@@ -1,17 +1,38 @@
-include("./Parameters/parameter_mesh_handling.jl")
-include("./Parameters/parameter_handling_blocks.jl")
-#module Parameter_Handling
+
+module Data_manager
+include("./Parameters/parameter_handling.jl")
 #export nnodes
 #export nbonds
-#export create_node_field
-function check_element(data, key)
-    return haskey(data, key)
+export set_nnodes
+export get_nnodes
+export set_nbonds
+export get_nbonds
+export create_node_field
+export create_constant_node_field
+export create_bond_field
+export create_constant_bond_field
+##########################
+# Variables
+##########################
+nnodes = Ref(0)
+nbonds = Ref(0)
+fieldnames = Dict(Int64 => Dict(), Float32 => Dict(), Bool => Dict())
+##########################
+
+
+function set_nnodes(n)
+    global nnodes = n
+end
+function get_nnodes()
+    return nnodes
 end
 
-global fieldnames = Dict(Int64 => Dict(), Float32 => Dict(), Bool => Dict())
-global nnodes::Int64 = 0 # where to put?
-global nbonds::Int64 = 0 # where to put?
-
+function set_nbonds(n)
+    global nbonds = n
+end
+function get_nbonds()
+    return nbonds
+end
 function create_node_field(name::String, type::DataType, dof::Int64)
     return create_field(name, type, "Node", dof, true)
 end
@@ -29,33 +50,33 @@ end
 # nicht synchronisierte vectoren brauchen nicht hier rein -> Problem ist, wie mit der synch Option zu verfahren ist (eine stelle synch andere non synch)Was zählt?
 # besser ist vielleicht einen synchmanager zu bauen -> dort kann man sich eintragen
 
-function create_field(name::String, type::DataType, bondNode::String, dof::Int64, time_dependend::Bool)
+function create_field(name::String, vartyp::DataType, bondNode::String, dof::Int64, time_dependend::Bool)
 
     if !haskey(fieldnames, type)
-        fieldnames[type] = Dict()
+        fieldnames[vartyp] = Dict()
     end
-    if haskey(fieldnames[type], name)
-        return fieldnames[type][name]
+    if haskey(fieldnames[vartyp], name)
+        return fieldnames[vartyp][name]
     else
         len = field_length(bondNode, time_dependend) * dof
 
-        if length(values(fieldnames[type])) == 0
+        if length(values(fieldnames[vartyp])) == 0
             num = 0
         else
             # start ende -> alles in einen Vector, dann sind weniger synchros nötig
-            num = maximum(values(fieldnames[type]))[2]
+            num = maximum(values(fieldnames[vartyp]))[2]
             fieldnames[type][name] = (num + 1, num + len + 1)
         end
-        return fieldnames[type][name]
+        return fieldnames[vartyp][name]
     end
 end
 
 function field_length(bondNode::String, time_dependend::Bool)
     le = 0
     if bondNode == "Bond"
-        le = nbonds
+        le = Parameter_Handling.nbonds
     elseif bondNode == "Node"
-        le = nnodes
+        le = Parameter_Handling.nnodes
     else
         le = 0
         @error "Not supported option $bondNode allowed options are Bond::String or Node::String"
@@ -79,4 +100,4 @@ function synch_manager()
     # download sum; für time int
     # down - up für bond information
 end
-#end
+end
