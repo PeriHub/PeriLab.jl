449d7e25089cce8882b3982d1bb86591b07c75bc
will_cr <christian.willberg@dlr.de>
2023-07-12 14:12:28 +0200

    update geometry module

--- src/Support/geometry.jl
+++ src/Support/geometry.jl
@@ -1,24 +1,14 @@
 
-function bond_geometry(data)
-    coordinates = data["coordinates"]
-    neigborlist = data["Neighborlist"]
-    bondDamage = data["Bond Damage"]
-    bondGeometry = data["Bond Geometry"] #dx,dy,dz,len
-    invK = data["Inverse Shape Tensor"]
-
-    nnodes = data.nnodes
-    dof = data.dof
+module Geometry
+export bond_geometry
+function bond_geometry(nnodes, dof, nlist, coor, bondgeom)
     for iID in 1:nnodes
-        nneighbors = length(neigborlist[iID])
-        for jID in 1:nneighbors
-            pythagoras = 0.0
-            for i in 1:dof
-                bondGeometry[i] = coordinates[(iID-1)*dof+i] - coordinates[(neigborlist[jID]-1)*dof+i]
-                pythagoras += bondGeometry[i] * bondGeometry[i]
-            end
-            bondGeometry[dof+1] = sqrt(pythagoras)
+        nneigbors = length(nlist[iID])
+        for jID in 1:nneigbors
+            bondgeom[iID, jID, 1:dof] = coor[nlist[iID, jID], :] - coor[iID, :]
+            bondgeom[iID, jID, dof+1] = norm(bondgeom[iID, jID, 1:dof])
         end
-
     end
-    return data
+    return bondgeom
+end
 end
