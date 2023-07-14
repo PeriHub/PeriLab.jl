aa847e2e007f106375a680e4dda3a8f63159dc69
will_cr <christian.willberg@dlr.de>
2023-07-14 12:34:19 +0200

    ut_get_mesh_name

--- src/Support/Parameters/test/ut_parameter_handling.jl
+++ src/Support/Parameters/test/ut_parameter_handling.jl
@@ -1,5 +1,6 @@
 include("../parameter_handling.jl")
 using Test
+using Random
 @testset "ut_check_key_elements" begin
     params = Dict()
     @test check_key_elements(params) == false
@@ -13,4 +14,11 @@ using Test
     @test check_key_elements(params) == false
     params = Dict("Damage" => Dict(), "Materials" => Dict(), "Discretization" => Dict(), "Blocks" => Dict(), "Solver" => Dict())
     @test check_key_elements(params) == true
+end
+@testset "ut_get_mesh_name" begin
+    params = Dict("Discretization" => Dict())
+    @test get_mesh_name(params) = nothing
+    name = randstring(12)
+    params = Dict("Discretization" => Dict("Input Mesh File" => name))
+    @test get_mesh_name(params) == name
 end
