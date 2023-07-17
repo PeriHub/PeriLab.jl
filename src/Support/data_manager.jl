3ef30262953985ba9224982f5fb1231096f566d0
will_cr <christian.willberg@dlr.de>
2023-07-17 15:47:44 +0200

    bond field

--- src/Support/data_manager.jl
+++ src/Support/data_manager.jl
@@ -45,10 +45,10 @@ function create_constant_node_field(name::String, type::DataType, dof::Int64)
     return create_field(name, type, "Node", dof, false)
 end
 function create_bond_field(name::String, type::DataType, dof::Int64)
-    return create_field(name, type, "Bond", dof, true)
+    return create_field_2(name, type, "Bond", dof, true)
 end
 function create_constant_bond_field(name::String, type::DataType, dof::Int64)
-    return create_field(name, type, "Bond", dof, false)
+    return create_field_2(name, type, "Bond", dof, false)
 end
 # wenn nicht existiert, wird an den gesamtvector der Teil angehängt. Dann wird der Vectorabschnitt zurückgeschickt und ist in der jeweiligen routine nutzbar
 # bisher wird alles synchrononisiert
@@ -85,6 +85,43 @@ function create_field(name::String, vartype::DataType, bondNode::String, dof::In
     end
 end
 
+function create_field_2(name::String, vartype::DataType, bondNode::String, dof::Int64, time_dependend::Bool)
+    """
+    create_field(name::String, vartype::DataType, bondNode::String, dof::Int64, time_dependend::Bool)
+
+    Create a field with the given `name` for the specified `vartype`. If the field already exists, return the existing field. If the field does not exist, create a new field with the specified characteristics.
+
+    # Arguments
+    - `name::String`: The name of the field.
+    - `vartype::DataType`: The data type of the field.
+    - `bondNode::String`: length of the field (nnodes or nbonds) associated with the field.
+    - `dof::Int64`: The degrees of freedom per node.
+    - `time_dependend::Bool`: Indicates whether the field is time-dependent.
+
+    # Returns
+    The field with the given `name` and specified characteristics.
+
+    """
+
+    if haskey(fieldnames[vartype], name)
+        return fieldnames[vartype][name]
+    else
+        len = field_length(bondNode, time_dependend)
+        nBonds = get_field("Number of Neighbors")
+
+        fieldnames[vartype][name] = fill([], nnodes)
+
+        for i in 1:nnodes
+            if dof == 1
+                fieldnames[vartype][name][i] = zeros(vartype, nBonds[i])
+            else
+                fieldnames[vartype][name][i] = zeros(vartype, nBonds[i], dof)
+            end
+        end
+
+        return fieldnames[vartype][name]
+    end
+end
 function get_field(name::String)
     """
     get_field(name::String)
