14fde3e1d4b90430f95e9ea939aa72cc3bd73637
will_cr <christian.willberg@dlr.de>
2023-07-18 16:48:15 +0200

    init solver

--- src/Core/Solver/Solver_control.jl
+++ src/Core/Solver/Solver_control.jl
@@ -1,4 +1,5 @@
 
+include("../../Support/Parameters/parameter_handling.jl")
 include("../../Physics/Physics_Factory.jl")
 import .Physics
 module Solver
@@ -7,10 +8,25 @@ module Solver
 function init(params, datamodel)
 
     blockNodes = get_blockNodes(datamodel.get_field("Block_Id"))
+    mechanical, thermal, additive = get_solver_optopms(params)
+    if mechanical
+        dof = datamanager.get_dof()
+        force = datamanager.create_node_field("Force", Float32, dof)
+        Y = datamanager.create_node_field("Deformed State", Float32, dof)
+        u = datamanager.create_node_field("Displacement", Float32, dof)
+        bu = datamanager.create_bond_field("Deformed Bond Geometry", Float32, dof + 1)
+    end
+    if thermal
+        temperature = datamanager.create_node_field("Temperature", Float32, 1)
+        flow = datamanager.create_node_field("Flow", Float32, dof) # -> check dof
+    end
+    if additive
+
+    end
     physics = Physics.get_physics(params)
 
 
-    return blockNodes
+    return blockNodes, datamodel
 end
 function get_blockNodes(blockID)
     maxBlock = maximum(blockID)
