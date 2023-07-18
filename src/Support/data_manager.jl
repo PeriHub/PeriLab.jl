d660f1a2721b456155164947361895491c12cf4d
will_cr <christian.willberg@dlr.de>
2023-07-18 06:49:38 +0200

    optimizing data handling

--- src/Support/data_manager.jl
+++ src/Support/data_manager.jl
@@ -39,34 +39,32 @@ function get_nbonds()
     return nbonds
 end
 function create_node_field(name::String, type::DataType, dof::Int64)
-    return create_field(name, type, "Node", dof, true)
+    return create_field(name * "N", type, dof), create_field(name * "NP1", type, dof)
 end
 function create_constant_node_field(name::String, type::DataType, dof::Int64)
-    return create_field(name, type, "Node", dof, false)
+    return create_field(name, type, dof)
 end
 function create_bond_field(name::String, type::DataType, dof::Int64)
-    return create_field_2(name, type, "Bond", dof, true)
+    return create_field_bond_type_field(name, type, dof)
 end
 function create_constant_bond_field(name::String, type::DataType, dof::Int64)
-    return create_field_2(name, type, "Bond", dof, false)
+    return create_field_bond_type_field(name * "N", type, dof), create_field_bond_type_field(name * "NP1", type, dof)
 end
 # wenn nicht existiert, wird an den gesamtvector der Teil angehängt. Dann wird der Vectorabschnitt zurückgeschickt und ist in der jeweiligen routine nutzbar
 # bisher wird alles synchrononisiert
 # nicht synchronisierte vectoren brauchen nicht hier rein -> Problem ist, wie mit der synch Option zu verfahren ist (eine stelle synch andere non synch)Was zählt?
 # besser ist vielleicht einen synchmanager zu bauen -> dort kann man sich eintragen
 
-function create_field(name::String, vartype::DataType, bondNode::String, dof::Int64, time_dependend::Bool)
+function create_field(name::String, vartype::DataType, dof::Int64)
     """
-    create_field(name::String, vartype::DataType, bondNode::String, dof::Int64, time_dependend::Bool)
+    create_field(name::String, vartype::DataType, bondNode::String, dof::Int64)
 
     Create a field with the given `name` for the specified `vartype`. If the field already exists, return the existing field. If the field does not exist, create a new field with the specified characteristics.
 
     # Arguments
     - `name::String`: The name of the field.
     - `vartype::DataType`: The data type of the field.
-    - `bondNode::String`: length of the field (nnodes or nbonds) associated with the field.
     - `dof::Int64`: The degrees of freedom per node.
-    - `time_dependend::Bool`: Indicates whether the field is time-dependent.
 
     # Returns
     The field with the given `name` and specified characteristics.
@@ -75,19 +73,19 @@ function create_field(name::String, vartype::DataType, bondNode::String, dof::In
     if haskey(fieldnames[vartype], name)
         return fieldnames[vartype][name]
     else
-        len = field_length(bondNode, time_dependend)
+
         if dof == 1
-            fieldnames[vartype][name] = zeros(vartype, len)
+            fieldnames[vartype][name] = zeros(vartype, nnodes)
         else
-            fieldnames[vartype][name] = zeros(vartype, len, dof)
+            fieldnames[vartype][name] = zeros(vartype, nnodes, dof)
         end
         return fieldnames[vartype][name]
     end
 end
 
-function create_field_2(name::String, vartype::DataType, bondNode::String, dof::Int64, time_dependend::Bool)
+function create_field_bond_type_field(name::String, vartype::DataType, dof::Int64)
     """
-    create_field(name::String, vartype::DataType, bondNode::String, dof::Int64, time_dependend::Bool)
+    create_field(name::String, vartype::DataType, dof::Int64)
 
     Create a field with the given `name` for the specified `vartype`. If the field already exists, return the existing field. If the field does not exist, create a new field with the specified characteristics.
 
@@ -96,7 +94,6 @@ function create_field_2(name::String, vartype::DataType, bondNode::String, dof::
     - `vartype::DataType`: The data type of the field.
     - `bondNode::String`: length of the field (nnodes or nbonds) associated with the field.
     - `dof::Int64`: The degrees of freedom per node.
-    - `time_dependend::Bool`: Indicates whether the field is time-dependent.
 
     # Returns
     The field with the given `name` and specified characteristics.
@@ -106,7 +103,7 @@ function create_field_2(name::String, vartype::DataType, bondNode::String, dof::
     if haskey(fieldnames[vartype], name)
         return fieldnames[vartype][name]
     else
-        len = field_length(bondNode, time_dependend)
+
         nBonds = get_field("Number of Neighbors")
 
         fieldnames[vartype][name] = fill([], nnodes)
@@ -118,7 +115,7 @@ function create_field_2(name::String, vartype::DataType, bondNode::String, dof::
                 fieldnames[vartype][name][i] = zeros(vartype, nBonds[i], dof)
             end
         end
-
+        #
         return fieldnames[vartype][name]
     end
 end
