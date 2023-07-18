74e3205024297fa468b6869e3363ce4ced05a547
will_cr <christian.willberg@dlr.de>
2023-07-18 16:41:33 +0200

    index error bug fix

--- src/Support/geometry.jl
+++ src/Support/geometry.jl
@@ -5,8 +5,8 @@ function bond_geometry(nnodes, dof, nlist, coor, bondgeom)
     for iID in 1:nnodes
         nneigbors = length(nlist[iID])
         for jID in 1:nneigbors
-            bondgeom[iID, jID, 1:dof] = coor[nlist[iID, jID], :] - coor[iID, :]
-            bondgeom[iID, jID, dof+1] = norm(bondgeom[iID, jID, 1:dof])
+            bondgeom[iID][jID, 1:dof] = coor[nlist[iID][jID], :] - coor[iID, :]
+            bondgeom[iID][jID, dof+1] = norm(bondgeom[iID][jID, 1:dof])
         end
     end
     return bondgeom
