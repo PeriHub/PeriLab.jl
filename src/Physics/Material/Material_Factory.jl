03c6cb6644eaed98210c974bce3658b6e519a673
will_cr <christian.willberg@dlr.de>
2023-07-27 07:20:03 +0200

    determine Material types

--- src/Physics/Material/Material_Factory.jl
+++ src/Physics/Material/Material_Factory.jl
@@ -4,6 +4,15 @@ include("../../Support/Parameters/parameter_handling.jl")
 module Material
 export get_material
 
+
+function material_types(params, datamanager)
+    material_types = get_material_types(params)
+    for material_type in material_types
+        datamanager.set_material_type(material_type, material_types[material_type])
+    end
+    return datamanager.get_material_type()
+end
+
 function get_material(params)
     materials = get_materials(params)
     material_names = keys(material)
@@ -13,15 +22,23 @@ function get_material(params)
 end
 
 
-function material_type(data)
+function material_type(params, datamanager)
     ## function for specific pre-calculations
-    if data.material.correspondence
+
+    if datamanager.is_material_type("Bond-Based")
+    end
+
+    if datamanager.is_material_type("Ordinary")
+    end
+
+    if datamanager.is_material_type("Correspondence")
         include("Correspondence.jl")
         data = shapeTensor(data)
         data = defGrad(data)
     end
-    if data.material.correspondence
+    if datamanager.is_material_type("Bond Associated")
     end
+
     return data
 end
 end
