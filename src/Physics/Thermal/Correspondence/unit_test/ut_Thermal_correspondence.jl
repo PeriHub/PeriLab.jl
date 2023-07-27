a90771e0b5f3429f29c880f513037ef0866d7f88
will_cr <christian.willberg@dlr.de>
2023-07-27 07:41:39 +0200

    thermal strain

---
+++ src/Physics/Thermal/Correspondence/unit_test/ut_Thermal_correspondence.jl
@@ -0,0 +1,12 @@
+using Test
+include("../Thermal_correspondence.jl")
+@testset "ut_check_key_elements" begin
+    strain = zeros(3, 3)
+    alpha = ones(3, 3)
+    temperature = 1.0
+    @test -alpha == thermal_strain(alpha, temperature, strain)
+    strain = ones(3, 3)
+    @test zeros(3, 3) == thermal_strain(alpha, temperature, strain)
+    @test ones(3, 3) == thermal_strain(alpha, 0.0, strain)
+
+end
