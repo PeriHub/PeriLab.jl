5cbdf2e4e5ddfcedffc9020477dbaf4ccd8de7d1
will_cr <christian.willberg@dlr.de>
2023-08-03 07:15:34 +0200

    coordinate style

--- src/IO/IO.jl
+++ src/IO/IO.jl
@@ -2,19 +2,52 @@ module IO
 
 import .Read_Input_Deck
 import .Read_Mesh
-import .Write_Results
+import .Write_Exodus_Results
+
 
 include("read_inputdeck.jl")
 include("mesh_data.jl")
 include("exodus_export.jl")
 export initialize_data
 
+function initialize_data(filename, datamanager, comm)
+    return Read_Mesh.init_data(read_input_file(filename), datamanager, comm)
+end
+
+
 function read_input_file(filename)
     return Read_Input_Deck.read_input_file(filename)
 end
 
-function initialize_data(filename, datamanager, comm)
-    return Read_Mesh.init_data(read_input_file(filename), datamanager, comm)
+function init_write_results(params, datamanager)
+    filenames = get_output_filenames(params)
+    dof = datamanager.get_dof()
+    coordinates = datamanager.get_field("Coordinates")'
+    block_Id = datamanager.get_field("Block_Id")
+    volume = datamanager.get_field("Volume")
+
+
+    if dof == 2
+        coords = [coordinates[:, 1], coordinates[:, 2]]
+    else
+        coords = [coordinates[:, 1], coordinates[:, 2], coordinates[:, 3]]
+    end
+
+    for file in filenames
+        init_write_results_in_exodus(file, coords, block_Id, volume)
+    end
+    # coords = [
+    #     1.0 0.5 0.5 1.0 0.0 0.0 0.5 1.0 0.0
+    #     1.0 1.0 0.5 0.5 1.0 0.5 0.0 0.0 0.0
+    #     0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
+    # ]
+    return filenames
+end
+
+function write_results(params, datamanager)
+
+
+
 end
 
 #export read_mesh
