3cea2cb9b286ec3e9a2febdd0db552365623ed14
will_cr <christian.willberg@dlr.de>
2023-08-05 08:29:12 +0200

    ut send_vector_from_root_to_core_i

--- src/MPI_communication/unit_test/ut_MPI.jl
+++ src/MPI_communication/unit_test/ut_MPI.jl
@@ -29,7 +29,25 @@ size = MPI.Comm_size(comm)
     send_msg = send_value(comm, 0, send_msg)
     @test send_msg == 100.5
 end
+@testset "ut_send_vector_from_root_to_core_i" begin
+    distribution = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+    if rank == 0
+        send_msg = [2, 1, 5]
+    else
+        send_msg = nothing
+    end
+    recv_msg = [0, 0, 0]
 
-
-
+    recv_msg = send_vector_from_root_to_core_i(comm, send_msg, recv_msg, distribution)
+    @test recv_msg[1] == 2
+    @test recv_msg[2] == 1
+    @test recv_msg[3] == 5
+    distribution = [[1, 2, 3], [3, 2, 1], [3, 2, 1]]
+    recv_msg = send_vector_from_root_to_core_i(comm, send_msg, recv_msg, distribution)
+    if rank != 0
+        @test recv_msg[1] == 5
+        @test recv_msg[2] == 1
+        @test recv_msg[3] == 2
+    end
+end
 MPI.Finalize()
