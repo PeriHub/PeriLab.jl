0d0b66957ee9321ef8ab08dbf909110ca20054ea
will_cr <christian.willberg@dlr.de>
2023-08-14 07:25:03 +0200

    delete nsets from bc

--- src/Support/Parameters/parameter_handling_bc.jl
+++ src/Support/Parameters/parameter_handling_bc.jl
@@ -1,21 +1,5 @@
 using CSV
 using DataFrames
-function get_bc_node_sets(params)
-
-    nsets = Dict{String,Any}()
-    if check_element(params, "Boundary Conditions") && check_element(params, "Node Sets")
-
-        for entry in params["Boundary Conditions"]["Node Sets"]
-            if occursin(".txt", params["Boundary Conditions"]["Node Sets"][entry])
-                nodes = CSV.read(params["Boundary Conditions"]["Node Sets"][entry], DataFrame; delim=" ", header=false)
-                nsets[entry] = nodes.Column1
-            else
-                nsets[entry] = params["Boundary Conditions"]["Node Sets"][entry]
-            end
-        end
-    end
-    return nsets
-end
 
 function get_bc_node_definitions(params)
     bcs = Dict{String,Any}()
