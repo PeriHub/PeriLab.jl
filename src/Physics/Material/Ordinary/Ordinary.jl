c740c884a69d7e126a9a3030496e7b0073be0058
will_cr <christian.willberg@dlr.de>
2023-08-15 15:40:24 +0200

    bug fix and code improvements

--- src/Physics/Material/Ordinary/Ordinary.jl
+++ src/Physics/Material/Ordinary/Ordinary.jl
@@ -1,39 +1,36 @@
-function compute_weighted_volume(nnodes, bondlength, nlist, bonddamage, omega, volume, weightedVol)
-
+function compute_weighted_volume(nnodes, nlist, bond_geometry, bond_damage, omega, volume)
+    weighted_volume = zeros(nnodes)
     for iID in 1:nnodes
-        nneighbors = length(nlist[iID])
-        weightedVol[iID] = 0.0
-        for jID in 1:nneighbors
-            localID = nlist[iID, jID]
-            weightedVol[iID] += omega[localID] * bonddamage[localID, jID] * bondlength[iID, jID, end] * bondlength[iID, jID, end] * volume[localID]
+        nbonds = size(bond_geometry[iID])[1]
+        for jID in 1:nbonds
+            weighted_volume[iID] += omega[iID][jID] * bond_damage[iID][jID] * bond_geometry[iID][jID, end] * bond_geometry[iID][jID, end] * volume[nlist[iID][jID]]
         end
 
     end
-    return m
+    return weighted_volume
 end
 
-function compute_dilatation(nnodes, bond_geometry, bond_stretch, bond_damage, volume, weighted_volume, omega, dof)
+function compute_dilatation(nnodes, bond_geometry, bond_stretch, bond_damage, volume, weighted_volume, omega)
     theta = zeros(nnodes)
     for iID in 1:nnodes
-        nbonds = length(bond_geometry)
+        nbonds = size(bond_geometry[iID])[1]
         for jID in 1:nbonds
-            e = bond_stretch[iID][jID, dof+1] - bond_geometry[iID][jID, dof+1]
-            theta[iID] += 3.0 * omega[iID][jID] * bond_damage[iID][jID] * bond_geometry[iID][jID, dof+1] * e * volume[iID] / weighted_volume[iID]
+            e = bond_stretch[iID][jID, end] - bond_geometry[iID][jID, end]
+            theta[iID] += 3.0 * omega[iID][jID] * bond_damage[iID][jID] * bond_geometry[iID][jID, end] * e * volume[nlist[iID][jID]] / weighted_volume[iID]
         end
     end
     return theta
 end
 
-function elastic(nnodes, bond_geometry, bond_stretch, bond_damage, theta, weighted_volume, omega, dof)
+function elastic(nnodes, bond_geometry, bond_stretch, bond_damage, theta, weighted_volume, omega)
 
     for iID in 1:nnodes
         alpha = 15.0 * MU / weighted_volume[iID]
         beta = 3.0 * K / weighted_volume[iID]
-        nbonds = length(bond_geometry)
+        nbonds = size(bond_geometry[iID])[1]
         for jID in 1:nbonds
-
             c1 = omega * theta[iID] * (beta - alpha / 3.0)
-            bond_force[iID][jID] = bond_damage[iID][jID] * omega[iID][jID] * (c1 * bond_geometry[iID][jID, dof+1] + alpha * bond_stretch[iID][jID, dof+1])
+            bond_force[iID][jID] = bond_damage[iID][jID] * omega[iID][jID] * (c1 * bond_geometry[iID][jID, end] + alpha * bond_stretch[iID][jID, end])
         end
     end
     return bond_force
