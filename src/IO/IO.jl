d721e78ec7d9c537a13cd569cc5ac76b0614f8c7
will_cr <christian.willberg@dlr.de>
2023-08-17 11:50:41 +0200

    bug fixes

--- src/IO/IO.jl
+++ src/IO/IO.jl
@@ -4,16 +4,15 @@ import .Read_Input_Deck
 import .Read_Mesh
 import .Write_Exodus_Results
 
-
 include("read_inputdeck.jl")
 include("mesh_data.jl")
 include("exodus_export.jl")
+include("../Support/Parameters/parameter_handling.jl")
 export initialize_data
-
+export init_write_results
 function initialize_data(filename, datamanager, comm)
     return Read_Mesh.init_data(read_input_file(filename), datamanager, comm)
 end
-
 
 function read_input_file(filename)
     return Read_Input_Deck.read_input_file(filename)
@@ -27,19 +26,14 @@ function init_write_results(params, datamanager)
     nnsets = datamanager.get_nnsets()
     coordinates = datamanager.get_field("Coordinates")'
     block_Id = datamanager.get_field("Block_Id")
-    nsets = datamanager.get_node_sets()
+    nsets = datamanager.get_nsets()
     for filename in filenames
-        append!(exos, Write_Exodus_Results.init_result_file(filename, nnodes, dof, maximum(block_Id), length(nsets)))
+        append!(exos, Write_Exodus_Results.create_result_file(filename, nnodes, dof, maximum(block_Id), length(nsets)))
     end
     outputs = get_outputs(params, datamanager.get_all_field_keys())
-    if dof == 2
-        coords = vcat(coordinates[:, 1], coordinates[:, 2])
-    else
-        coords = vcat(coordinates[:, 1], coordinates[:, 2], coordinates[:, 3])
-    end
-
+    coords = vcat(transpose(coordinates))
     for num in 1:length(exos)
-        exos[i] = init_results_in_exodus(exos[i], outputs[i], coordinates, block_Id, volume)
+        exos[i] = init_results_in_exodus(exos[i], outputs[i], coords, block_Id, volume)
     end
 
     return exos
