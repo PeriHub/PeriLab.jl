4c545ad8827b1773f5504da8ba4ba94b90137935
Jan-Timo Hesse <jan-timo.hesse@dlr.de>
2023-08-23 14:56:54 +0200

    added testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,93 @@
-@test π ≈ 3.14 atol = 0.01
-# Test Passed
+using PeriLab
+using Test
+using Aqua
+
+@testset "Core" begin
+
+
+    @testset "Solver" begin
+
+        @testset "ut_Solver_control" begin
+            include("../src/Core/Solver/unit_test/ut_Solver_control.jl")
+        end
+
+        @testset "ut_Verlet" begin
+            include("../src/Core/Solver/unit_test/ut_Verlet.jl")
+        end
+
+    end
+
+    @testset "ut_BC_manager" begin
+        include("../src/Core/unit_test/ut_BC_manager.jl")
+    end
+end
+
+@testset "IO" begin
+
+    @testset "ut_exodus_export" begin
+        include("../src/IO/unit_test/ut_exodus_export.jl")
+    end
+
+    @testset "ut_IO" begin
+        include("../src/IO/unit_test/ut_IO.jl")
+    end
+
+    @testset "ut_mesh_data" begin
+        include("../src/IO/unit_test/ut_mesh_data.jl")
+    end
+end
+
+
+@testset "MPI" begin
+
+    @testset "ut_MPI" begin
+        include("../src/MPI_communication/unit_test/ut_MPI.jl")
+    end
+
+end
+
+
+@testset "Physics" begin
+
+    @testset "Thermal" begin
+
+        @testset "Correspondence" begin
+
+            @testset "ut_MPI" begin
+                include("../src/Physics/Thermal/Correspondence/unit_test/ut_Thermal_correspondence.jl")
+            end
+
+        end
+    end
+
+    @testset "ut_Physics_Factory" begin
+        include("../src/Physics/unit_test/ut_Physics_Factory.jl")
+    end
+
+end
+
+@testset "Support" begin
+
+    @testset "Parameters" begin
+
+        @testset "ut_parameter_handling" begin
+            include("../src/Support/Parameters/unit_test/ut_parameter_handling.jl")
+        end
+
+    end
+
+    @testset "ut_data_manager" begin
+        include("../src/Support/unit_test/ut_data_manager.jl")
+    end
+
+    @testset "ut_helpers" begin
+        include("../src/Support/unit_test/ut_helpers.jl")
+    end
+
+    @testset "ut_tools" begin
+        include("../src/Support/unit_test/ut_tools.jl")
+    end
+
+end
+
+Aqua.test_all(PeriLab)
