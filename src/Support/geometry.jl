f211b89d02a9b0d37d123bb2f7610158ae375e50
will_cr <christian.willberg@dlr.de>
2023-08-31 08:28:25 +0200

    distance error

--- src/Support/geometry.jl
+++ src/Support/geometry.jl
@@ -6,6 +6,9 @@ function bond_geometry(nnodes, dof, nlist, coor, bondgeom)
         for jID in eachindex(nlist[iID])
             bondgeom[iID][jID, 1:dof] = coor[nlist[iID][jID], :] - coor[iID, :]
             bondgeom[iID][jID, dof+1] = norm(bondgeom[iID][jID, 1:dof])
+            if bondgeom[iID][jID, dof+1] == 0
+                @error "Identical point coordinates with no distance $iID, $jID"
+            end
         end
     end
     return bondgeom
