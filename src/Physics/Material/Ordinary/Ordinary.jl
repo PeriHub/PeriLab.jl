c67f8e9505eeddd6b4f4358067c5386effceb55d
will_cr <christian.willberg@dlr.de>
2023-09-11 06:39:49 +0200

    fix

--- src/Physics/Material/Ordinary/Ordinary.jl
+++ src/Physics/Material/Ordinary/Ordinary.jl
@@ -8,12 +8,14 @@ function material_name()
     return "PD Solid Elastic"
 end
 
-function compute_weighted_volume(nnodes, nneighbors, nlist, bond_geometry, bond_damage, omega, volume)
+function compute_weighted_volume(nodes, nneighbors, nlist, bond_geometry, bond_damage, omega, volume)
     """
     taken from Peridigm
     """
-    weighted_volume = zeros(Float32, nnodes)
-    for iID in nnodes
+    # not optimal, because of many zeros, but simpler, because it avoids reorganization. Part of potential optimization
+    weighted_volume = zeros(Float32, maximum(nodes))
+
+    for iID in nodes
         for jID in 1:nneighbors[iID]
             weighted_volume[iID] += omega[iID] * bond_damage[iID][jID] * bond_geometry[iID][jID, end] * bond_geometry[iID][jID, end] * volume[nlist[iID][jID]]
         end
@@ -21,9 +23,10 @@ function compute_weighted_volume(nnodes, nneighbors, nlist, bond_geometry, bond_
     return weighted_volume
 end
 
-function compute_dilatation(nnodes, nneighbors, nlist, bond_geometry, deformed_bond, bond_damage, volume, weighted_volume, omega)
-    theta = zeros(Float32, nnodes)
-    for iID in nnodes
+function compute_dilatation(nodes, nneighbors, nlist, bond_geometry, deformed_bond, bond_damage, volume, weighted_volume, omega)
+    # not optimal, because of many zeros, but simpler, because it avoids reorganization. Part of potential optimization
+    theta = zeros(Float32, maximum(nodes))
+    for iID in nodes
         theta[iID] = 3.0 * omega[iID] * sum(
                          bond_damage[iID][jID] * bond_geometry[iID][jID, end] *
                          (deformed_bond[iID][jID, end] - bond_geometry[iID][jID, end]) *
