11decb0544626580fc4fe3b1a67fddb56a6872de
will_cr <christian.willberg@dlr.de>
2023-09-12 17:02:19 +0200

    new test

--- test/multifunctional_test/test_MPI_data_manager.jl
+++ test/multifunctional_test/test_MPI_data_manager.jl
@@ -1,7 +1,9 @@
 include("../../src/Support/data_manager.jl")
+include("../../src/IO/mesh_data.jl")
 using Test
 using MPI
 using .Data_manager
+using .Read_Mesh
 include("../../src/MPI_communication/MPI_communication.jl")
 MPI.Init()
 testDatamanager = Data_manager
@@ -24,8 +26,16 @@ nlist = testDatamanager.create_constant_bond_field("Neighborhoodlist", Int64, 1)
         @test length(list) == nneighbors[id]
     end
 end
+distribution = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [7, 8, 9, 10, 11, 12, 3, 4, 5, 6]]
 
+if rank == 0
+    send_msg = [2, 3, 3, 2, 3, 4, 4, 3, 2, 3, 3, 2]
+end
+glob_to_loc = Read_Mesh.glob_to_loc(distribution)
 @testset "init_data rank $rank" begin
+    lenNlist[:] = send_vector_from_root_to_core_i(comm, send_msg, lenNlist, distribution)
+    nlistCore = datamanager.create_constant_bond_field("Neighborhoodlist", Int64, 1)
 
+    println("nlistcore ", MPI.Comm_rank(comm), " ", nlistCore)
 end
 MPI.Finalize()
