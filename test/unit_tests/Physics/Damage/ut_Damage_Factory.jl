0c5b6dab98d862afeae58a9239eee9b06e7a203a
will_cr <christian.willberg@dlr.de>
2023-09-19 10:25:25 +0200

    damage_index

---
+++ test/unit_tests/Physics/Damage/ut_Damage_Factory.jl
@@ -0,0 +1,54 @@
+include("../../../../src/Physics/Damage/Damage_Factory.jl")
+include("../../../../src/Support/data_manager.jl")
+using Test
+using .Damage
+@testset "damage_index" begin
+    testDatamanager = Data_manager
+    testDatamanager.set_nmasters(3)
+    nn = testDatamanager.create_constant_node_field("Number of Neighbors", Int64, 1)
+    nn[1] = 1
+    nn[2] = 2
+    nn[3] = 1
+    damageN, damageNP1_test = testDatamanager.create_node_field("Damage", Float32, 1)
+    volume = testDatamanager.create_constant_node_field("Volume", Float32, 1)
+    nlist = testDatamanager.create_constant_bond_field("Neighborhoodlist", Int64, 1)
+    bdN, bdNP1 = testDatamanager.create_bond_field("Bond Damage", Float32, 1)
+    nlist[1] = [2]
+    nlist[2] = [1, 3]
+    nlist[3] = [1]
+    volume[:] = [1.0, 2.0, 3.0]
+    bdNP1[1][:] .= 1
+    bdNP1[2][:] .= 1
+    bdNP1[3][:] .= 1
+    Damage.damage_index(1:3, testDatamanager)
+    @test damageNP1_test[1] == 0
+    @test damageNP1_test[2] == 0
+    @test damageNP1_test[3] == 0
+    bdNP1[1][:] .= 0
+    Damage.damage_index(1:3, testDatamanager)
+    @test damageNP1_test[1] == 1
+    @test damageNP1_test[2] == 0
+    @test damageNP1_test[3] == 0
+    bdNP1[2][1] = 0
+    Damage.damage_index(1:3, testDatamanager)
+    @test damageNP1_test[1] == 1
+    @test damageNP1_test[2] == 0.25
+    @test damageNP1_test[3] == 0
+    bdNP1[2][1] = 1
+    bdNP1[2][2] = 0
+    Damage.damage_index(1:3, testDatamanager)
+    @test damageNP1_test[1] == 1
+    @test damageNP1_test[2] == 0.75
+    @test damageNP1_test[3] == 0
+    bdNP1[2][1] = 0
+    bdNP1[2][2] = 0
+    Damage.damage_index(1:3, testDatamanager)
+    @test damageNP1_test[1] == 1
+    @test damageNP1_test[2] == 1
+    @test damageNP1_test[3] == 0
+    bdNP1[3][:] .= 0
+    Damage.damage_index(1:3, testDatamanager)
+    @test damageNP1_test[1] == 1
+    @test damageNP1_test[2] == 1
+    @test damageNP1_test[3] == 1
+end
