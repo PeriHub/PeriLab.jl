1f3e9e5f4bec165f3730ed8a562f35bf1e119ef8
will_cr <christian.willberg@dlr.de>
2023-09-25 14:12:51 +0200

    get_Hook_matrix

--- src/Physics/Material/material_basis.jl
+++ src/Physics/Material/material_basis.jl
@@ -1,10 +1,10 @@
 
 
-function get_all_elastic_moduli(parameter)
-    bulkM::Float32 = 0
+function get_all_elastic_moduli(parameter::Dict{String,Float32})
+    K::Float32 = 0
     E::Float32 = 0
     nu::Float32 = 0
-    ShearM::Float32 = 0
+    G::Float32 = 0
     bulk = false
     Youngs = false
     shear = false
@@ -18,11 +18,11 @@ function get_all_elastic_moduli(parameter)
         Youngs = true
     end
     if "Shear Modulus" in keys(parameter)
-        nu = parameter["Shear Modulus"]
+        G = parameter["Shear Modulus"]
         shear = true
     end
     if "Poisson's Ratio" in keys(parameter)
-        G = parameter["Poisson's Ratio"]
+        nu = parameter["Poisson's Ratio"]
         Poissons = true
     end
     if bulk && Poissons
@@ -57,15 +57,86 @@ function get_all_elastic_moduli(parameter)
     if bulk + Youngs + shear + Poissons < 2
         @error "Minimum of two parameters are needed for isotropic material"
     end
+
+    if bulk + Youngs + shear + Poissons > 2
+        @warn "More than two parameters for isotropic material. Please check consistency"
+    end
+
     parameter["Bulk Modulus"] = K
     parameter["Young's Modulus"] = E
     parameter["Shear Modulus"] = G
     parameter["Poisson's Ratio"] = nu
     return parameter
 end
-function get_Hook_matrix(parameter, dof)
-    matrix = zeros(Float32, dof, dof)
 
+function get_Hook_matrix(parameter, symmetry, dof)
+    """https://www.efunda.com/formulae/solid_mechanics/mat_mechanics/hooke_plane_stress.cfm"""
+    matrix = zeros(Float32, dof, dof)
+    if occursin("isotropic", symmetry)
+        nu = parameter["Poisson's Ratio"]
+        E = parameter["Young's Modulus"]
+        G = parameter["Shear Modulus"]
+        if occursin("3D", symmetry)
+            temp = 1 / ((1 + nu) * (1 - 2 * nu))
+            matrix[1, 1] = E * (1 - nu) * temp
+            matrix[2, 2] = E * (1 - nu) * temp
+            matrix[3, 3] = E * (1 - nu) * temp
+            matrix[1, 2] = E * nu * temp
+            matrix[1, 3] = E * nu * temp
+            matrix[2, 1] = E * nu * temp
+            matrix[1, 1] = E * nu * temp
+            matrix[3, 2] = E * nu * temp
+            matrix[2, 3] = E * nu * temp
+            matrix[4, 4] = G
+            matrix[5, 5] = G
+            matrix[6, 6] = G
+            return matrix
+        elseif occursin("plain_strain", symmetry)
+            matrix[1, 1] = E * (1 - nu) * temp
+            matrix[2, 2] = E * (1 - nu) * temp
+            matrix[3, 3] = G
+            matrix[1, 2] = E * nu * temp
+            matrix[2, 1] = E * nu * temp
+            return matrix
+        elseif occursin("plain_stress", symmetry)
+            matrix[1][1] = E / (1 - nu * nu)
+            matrix[2][2] = E * nu / (1 - nu * nu)
+            matrix[2][1] = E * nu / (1 - nu * nu)
+            matrix[2][2] = E / (1 - nu * nu)
+            matrix[3][3] = G
+            return matrix
+        end
+        if occursin("isotropic", symmetry)
+            anisoMatrix = zeros(Float32, 3, 3)
+            for iID in 1:6
+                for jID in iID:6
+                    if "C" * string(iID) * string(jID) in keys(parameter)
+                        value = parameter["C"*string(iID)*string(jID)]
+                    else
+                        value = 0
+                    end
+                    anisoMatrix[iID, jID] = value
+                    anisoMatrix[jID, iID] = value
+                end
+            end
+            if occursin("3D", symmetry)
+                return anisoMatrix
+            elseif occursin("plain_strain", symmetry)
+                matrix[1:2, 1:2] = anisoMatrix[1:2, 1:2]
+                matrix[3, 1:2] = anisoMatrix[6, 1:2]
+                matrix[1:2, 3] = anisoMatrix[1:2, 6]
+                matrix[3, 3] = anisoMatrix[6, 6]
+                return matrix
+            elseif occursin("plain_stress", symmetry)
+                invAniso = inv(anisoMatrix)
+                matrix[1:2, 1:2] = invAniso[1:2, 1:2]
+                matrix[3, 1:2] = invAniso[6, 1:2]
+                matrix[1:2, 3] = invAniso[1:2, 6]
+                matrix[3, 3] = invAniso[6, 6]
+                return inv(matrix)
+            end
+        end
+    end
     return matrix
 end
 
