023a518d466fee883f5afd03364d234e439e321a
will_cr <christian.willberg@dlr.de>
2023-10-03 08:10:00 +0200

    solve #39

--- src/Physics/Thermal/Thermal_Factory.jl
+++ src/Physics/Thermal/Thermal_Factory.jl
@@ -3,9 +3,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 module Thermal
-export get_thermal
 
-function get_thermal(params)
+include("../../Core/Module_inclusion/set_Modules.jl")
+using .Set_module
+global module_list = Set_modules.find_module_files(@__DIR__, "thermal_model_name")
+Set_modules.include_files(module_list)
 
+export compute_thermal_model
+
+function compute_thermal_model(datamanager, nodes, model_param, time, dt)
+
+    specifics = Dict{String,String}("Call Function" => "compute_thermal_model", "Name" => "thermal_model_name")
+
+    datamanager = Set_modules.create_module_specifics(model_param["Thermal Model"], module_list, specifics, (datamanager, nodes, model_param, time, dt))
+    if isnothing(datamanager)
+        @error "No thermal model of name " * model_param["Thermal Model"] * " exists."
+    end
+
+    return datamanager
 end
+
 end
