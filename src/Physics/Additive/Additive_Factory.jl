541248b0162afb626e53b50e6c8ea4a9c9af3786
will_cr <christian.willberg@dlr.de>
2023-10-03 08:05:29 +0200

    solved  #40

--- src/Physics/Additive/Additive_Factory.jl
+++ src/Physics/Additive/Additive_Factory.jl
@@ -3,8 +3,23 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 module Additive
-export get_additive
+include("../../Core/Module_inclusion/set_Modules.jl")
+using .Set_module
+global module_list = Set_modules.find_module_files(@__DIR__, "additive_name")
+Set_modules.include_files(module_list)
+export compute_additive
 
 
+function compute_additive(datamanager, nodes, model_param, time, dt)
+
+    specifics = Dict{String,String}("Call Function" => "compute_additive", "Name" => "additive_name")
+
+    datamanager = Set_modules.create_module_specifics(model_param["Additive Model"], module_list, specifics, (datamanager, nodes, model_param, time, dt))
+    if isnothing(datamanager)
+        @error "No additive model of name " * model_param["Additive Model"] * " exists."
+    end
+
+    return datamanager
+end
 
 end
