ea61b177aabc604b4cf6d9ec116bd76961c86b23
Jan-Timo Hesse <jan-timo.hesse@dlr.de>
2023-10-04 15:14:18 +0200

    Fixed tests and added cmd

---
+++ test/test_PD_Solid_Elastic/test_PD_Solid_Elastic.cmd
@@ -0,0 +1,14 @@
+@REM SPDX-FileCopyrightText: 2023 Christian Willberg <christian.willberg@dlr.de>, Jan-Timo Hesse <jan-timo.hesse@dlr.de>
+@REM
+@REM SPDX-License-Identifier: BSD-3-Clause
+
+DEFAULT TOLERANCE absolute 1.0E-9
+COORDINATES absolute 1.0E-12
+TIME STEPS absolute 1.0E-14
+NODAL VARIABLES absolute 1.0E-12
+	DisplacementX   absolute 1.0E-9
+	DisplacementY   absolute 1.0E-9
+	DisplacementZ   absolute 1.0E-9
+	VelocityX       absolute 5.0E-8
+	VelocityY       absolute 5.0E-8
+	VelocityZ       absolute 5.0E-8
