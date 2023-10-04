9e8cf1e698bbb8bb96770dd01b4ddd586aea14a5
Jan-Timo Hesse <jan-timo.hesse@dlr.de>
2023-10-04 11:11:53 +0200

    fixed Set_modules

--- src/Physics/Thermal/Thermal_Factory.jl
+++ src/Physics/Thermal/Thermal_Factory.jl
@@ -5,7 +5,7 @@
 module Thermal
 
 include("../../Core/Module_inclusion/set_Modules.jl")
-using .Set_module
+using .Set_modules
 global module_list = Set_modules.find_module_files(@__DIR__, "thermal_model_name")
 Set_modules.include_files(module_list)
 
