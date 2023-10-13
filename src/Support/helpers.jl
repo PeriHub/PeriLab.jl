9796074016c4d06f0072ba5776b1ea2acf139f9e
will_cr <christian.willberg@dlr.de>
2023-10-13 11:24:58 +0200

    code cleaning

--- src/Support/helpers.jl
+++ src/Support/helpers.jl
@@ -2,15 +2,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-function dof_index(index, inds)
-    dest = Vector{eltype(index)}(undef, inds * length(index))
-    i = 1
-    @inbounds for a in index, b in 1:inds
-        dest[i+=1] = a * inds + b - inds
-    end
-    dest
-end
-
 function find_indices(vector, what)
     return findall(item -> item == what, vector)
 end
