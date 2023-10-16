5cbe991ac82c015b902e15b1ead4e34f59c6e059
will_cr <christian.willberg@dlr.de>
2023-10-16 07:46:12 +0200

    typo

--- src/Physics/Thermal/Thermal_template/thermal_flow_template.jl
+++ src/Physics/Thermal/Thermal_template/thermal_flow_template.jl
@@ -25,7 +25,7 @@ function thermal_model_name()
   return "Thermal Template"
 end
 """
-   compute_force(datamanager, nodes, flow_parameter, time, dt)
+   compute_thermal_model(datamanager, nodes, flow_parameter, time, dt)
 
    Calculates the thermal behavior of the material. This template has to be copied, the file renamed and edited by the user to create a new flow. Additional files can be called from here using include and `import .any_module` or `using .any_module`. Make sure that you return the datamanager.
 
