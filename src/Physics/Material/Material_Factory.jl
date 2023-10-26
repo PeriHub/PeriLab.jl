ee21ab550b7ff3713adbff3ec2bc8cbebf83bd6b
Jan-Timo Hesse <jan-timo.hesse@dlr.de>
2023-10-26 09:49:25 +0200

    Fixed compute_forces

--- src/Physics/Material/Material_Factory.jl
+++ src/Physics/Material/Material_Factory.jl
@@ -16,7 +16,7 @@ function compute_forces(datamanager::Module, nodes::Union{SubArray,Vector{Int64}
     specifics = Dict{String,String}("Call Function" => "compute_forces", "Name" => "material_name")
     material_model_params = split(model_param["Material Model"], "+")
     for material_params in material_model_params
-        model_name = Set_modules.create_module_specifics(model_name, module_list, specifics, (datamanager, nodes, model_param, time, dt))
+        model_name = Set_modules.create_module_specifics(material_params, module_list, specifics, (datamanager, nodes, model_param, time, dt))
 
         if isnothing(datamanager)
             @error "No material of name " * model_name * " exists."
