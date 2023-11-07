efff342a2fabd9837923c4144375b4c9d7aa38d4
will_cr <christian.willberg@dlr.de>
2023-11-07 08:01:07 +0100

    minir fix

--- test/unit_tests/Support/Parameters/ut_parameter_handling.jl
+++ test/unit_tests/Support/Parameters/ut_parameter_handling.jl
@@ -12,11 +12,12 @@ using Test
 using Random
 
 
+
 @testset "ut_get_output_type" begin
     @test get_output_type(Dict("Output1" => Dict()), "Output1") == "Exodus"
     @test get_output_type(Dict("Output1" => Dict("Output Type" => "CSV")), "Output1") == "CSV"
     @test get_output_type(Dict("Output1" => Dict("Output Type" => "Exodus"), "Output2" => Dict("Output Type" => "CSV")), "Output1") == "Exodus"
-    @test get_output_type(Dict("Output1" => Dict("Output Type" => "Exodus")), "Output2" => Dict("Output Type" => "CSV"), "Output2") == "CSV"
+    @test get_output_type(Dict("Output1" => Dict("Output Type" => "Exodus"), "Output2" => Dict("Output Type" => "CSV")), "Output2") == "CSV"
     @test get_output_type(Dict("Output1" => Dict("Output Type" => "CSV"), "Output2" => Dict()), "Output2") == "Exodus"
     @test get_output_type(Dict("Output1" => Dict("Output Type" => "Exodus")), "Output1") == "Exodus"
 end
