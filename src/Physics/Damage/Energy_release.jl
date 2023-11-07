a64e6c7d5d49b3130185916d6db8088fed12af70
will_cr <christian.willberg@dlr.de>
2023-11-07 05:41:02 +0100

    error fix

--- src/Physics/Damage/Energy_release.jl
+++ src/Physics/Damage/Energy_release.jl
@@ -107,7 +107,7 @@ end
 function compute_damage_pre_calculation(datamanager::Module, nodes::Union{SubArray,Vector{Int64}}, block::Int64, synchronise_field, time::Float64, dt::Float64)
 
   #tbd thermal pre calculation
-  @timeit to "pre_calculation" datamanager = Pre_calculation.compute(datamanager, nodes, datamanager.get_physics_options(), time, dt)
+  datamanager = Pre_calculation.compute(datamanager, nodes, datamanager.get_physics_options(), time, dt)
   datamanager = Material.compute_forces(datamanager, nodes, datamanager.get_properties(block, "Material Model"), time, dt)
   datamanager = Material.distribute_force_densities(datamanager, nodes)
   synchronise_field(datamanager.get_comm(), datamanager.get_synch_fields(), datamanager.get_overlap_map(), datamanager.get_field, "Force DensitiesNP1", "download_from_cores")
