89c2163990fb4222eaeb15415c53e74fe3f04ca4
will_cr <christian.willberg@dlr.de>
2023-11-19 08:48:13 +0100

    thermal flow paper reference

---
+++ test/fullscale_tests/test_thermal_flow_paper/test_thermal_flow.jl
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2023 Christian Willberg <christian.willberg@dlr.de>, Jan-Timo Hesse <jan-timo.hesse@dlr.de>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+include("../../helper.jl")
+
+folder_name = basename(@__FILE__)[1:end-3]
+cd("fullscale_tests/" * folder_name) do
+    run_perilab("thermal_flow", 1, true, folder_name)
+end
