8d74b02b52e597f9e7e2371e4edf7179472fa240
will_cr <christian.willberg@dlr.de>
2023-11-22 16:29:20 +0100

    name fix

--- test/unit_tests/IO/ut_mesh_data.jl
+++ test/unit_tests/IO/ut_mesh_data.jl
@@ -259,7 +259,7 @@ end
     coor[2, 2] = 0
     coor[3, 1] = 0
     coor[3, 2] = 1
-    Read_Mesh.get_undeformed_bond(test_Data_manager)
+    Read_Mesh.get_bond_geometry(test_Data_manager)
     undeformed_bond = test_Data_manager.get_field("Bond Geometry")
 
     @test undeformed_bond[1][1, 1] == 1
