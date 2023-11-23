0556ce4ef40e6f82bd3dc63937b4d3fe5e12b7c7
will_cr <christian.willberg@dlr.de>
2023-11-23 09:02:31 +0100

    code efficiency

--- src/Physics/Physics_Factory.jl
+++ src/Physics/Physics_Factory.jl
@@ -84,7 +84,7 @@ function compute_models(datamanager::Module, block_nodes::Dict{Int64,Vector{Int6
         if options["Material Models"]
             if datamanager.check_property(block, "Material Model")
                 @timeit to "compute_bond_forces" datamanager = Material.compute_forces(datamanager, update_nodes, datamanager.get_properties(block, "Material Model"), time, dt)
-                datamanager = Material.distribute_force_densities(datamanager, update_nodes)
+                datamanager = Material.distribute_force_densities(datamanager, nodes)
             end
         end
     end
@@ -119,9 +119,6 @@ function compute_damage_pre_calculation(datamanager::Module, options::Dict, node
     if options["Material Models"]
         datamanager = Material.compute_forces(datamanager, nodes, datamanager.get_properties(block, "Material Model"), time, dt)
     end
-    datamanager = Material.distribute_force_densities(datamanager, nodes)
-    synchronise_field(datamanager.get_comm(), datamanager.get_synch_fields(), datamanager.get_overlap_map(), datamanager.get_field, "Force DensitiesNP1", "download_from_cores")
-    synchronise_field(datamanager.get_comm(), datamanager.get_synch_fields(), datamanager.get_overlap_map(), datamanager.get_field, "Force DensitiesNP1", "upload_to_cores")
     datamanager = Damage.compute_damage_pre_calculation(datamanager, nodes, block, datamanager.get_properties(block, "Damage Model"), synchronise_field, time, dt)
     update_list = datamanager.get_field("Update List")
     update_list[nodes] .= false
