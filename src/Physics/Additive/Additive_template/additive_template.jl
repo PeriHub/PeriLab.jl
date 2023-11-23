53b85343c6c0e65ae602884afcac12a7c7a7730f
Jan-Timo Hesse <jan-timo.hesse@dlr.de>
2023-11-23 17:07:59 +0100

    indent

--- src/Physics/Additive/Additive_template/additive_template.jl
+++ src/Physics/Additive/Additive_template/additive_template.jl
@@ -23,7 +23,7 @@ println(additive_name())
 ```
 """
 function additive_name()
-  return "Additive Template"
+    return "Additive Template"
 end
 
 """
@@ -44,12 +44,12 @@ Example:
   ```
 """
 function compute_additive(datamanager::Module, nodes::Union{SubArray,Vector{Int64}}, additive_parameter::Dict, time::Float64, dt::Float64)
-  @info "Please write a additive name in additive_name()."
-  @info "You can call your routine within the yaml file."
-  @info "Fill the compute_additive(datamanager, nodes, additive_parameter, time, dt) function."
-  @info "The datamanager and additive_parameter holds all you need to solve your problem on additive level."
-  @info "add own files and refer to them. If a module does not exist. Add it to the project or contact the developer."
-  return datamanager
+    @info "Please write a additive name in additive_name()."
+    @info "You can call your routine within the yaml file."
+    @info "Fill the compute_additive(datamanager, nodes, additive_parameter, time, dt) function."
+    @info "The datamanager and additive_parameter holds all you need to solve your problem on additive level."
+    @info "add own files and refer to them. If a module does not exist. Add it to the project or contact the developer."
+    return datamanager
 end
 
 end
