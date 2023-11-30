5fdd60ed2087a36c31135437f1770998e670ce72
Jan-Timo Hesse <jan-timo.hesse@dlr.de>
2023-11-30 18:09:34 +0100

    Removed a few tests

--- test/fullscale_tests/test_correspondence_elastic_with_zero_E_control/test_correspondence_elastic_with_zero_E_control.jl
+++ test/fullscale_tests/test_correspondence_elastic_with_zero_E_control/test_correspondence_elastic_with_zero_E_control.jl
@@ -6,10 +6,10 @@ include("../../helper.jl")
 
 folder_name = basename(@__FILE__)[1:end-3]
 cd("fullscale_tests/" * folder_name) do
-    run_perilab("correspondence_strain_pstrain_zc_xx", 1, true, folder_name)
+    # run_perilab("correspondence_strain_pstrain_zc_xx", 1, true, folder_name)
     run_perilab("correspondence_strain_pstrain_zc_xy", 1, true, folder_name)
-    run_perilab("correspondence_strain_pstrain_zc_yy", 1, true, folder_name)
-    run_perilab("correspondence_strain_pstress_zc_xx", 1, true, folder_name)
+    # run_perilab("correspondence_strain_pstrain_zc_yy", 1, true, folder_name)
+    # run_perilab("correspondence_strain_pstress_zc_xx", 1, true, folder_name)
     run_perilab("correspondence_strain_pstress_zc_xy", 1, true, folder_name)
-    run_perilab("correspondence_strain_pstress_zc_yy", 1, true, folder_name)
+    # run_perilab("correspondence_strain_pstress_zc_yy", 1, true, folder_name)
 end
