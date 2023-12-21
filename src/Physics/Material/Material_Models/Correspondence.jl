66d8ce09440f1df922d09e68eea83b7cac1465c8
will_cr <christian.willberg@dlr.de>
2023-12-21 18:24:22 +0100

    fix

--- src/Physics/Material/Material_Models/Correspondence.jl
+++ src/Physics/Material/Material_Models/Correspondence.jl
@@ -156,8 +156,10 @@ Global - J. Wan et al., "Improved method for zero-energy mode suppression in per
 - `datamanager::Data_manager`: Datamanager.
 """
 function zero_energy_mode_compensation(datamanager::Module, nodes::Union{SubArray,Vector{Int64}}, material_parameter::Dict, time::Float64, dt::Float64)
-  if !haskey(material_parameter, "Zero Energy Control") && time == 0
-    @warn "No zero energy control activated for corresponcence."
+  if !haskey(material_parameter, "Zero Energy Control")
+    if time == 0
+      @warn "No zero energy control activated for corresponcence."
+    end
     return datamanager
   end
   if material_parameter["Zero Energy Control"] == global_zero_energy_control.control_name()
