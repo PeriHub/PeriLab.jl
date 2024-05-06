e3d8292ea319d35171619f567aebba7bd53c5917
will_cr <christian.willberg@gmx.de>
2024-05-06 13:35:44 +0200

    solve #153 and #155

--- test/unit_tests/Physics/Material/Material_Models/ut_Bond_Associated_Correspondence_Material.jl
+++ test/unit_tests/Physics/Material/Material_Models/ut_Bond_Associated_Correspondence_Material.jl
@@ -6,25 +6,40 @@ include("../../../../../src/Physics/Material/Material_Models/Bond_Associated_Cor
 # include("../../../../../src/Support/data_manager.jl")
 using Test
 @testset "ut_find_local_neighbors" begin
-coordinates = zeros(Float64, 5, 2)
-coordinates[1, 1] = 0
-coordinates[1, 2] = 0
-coordinates[2, 1] = 1
-coordinates[2, 2] = 0
-coordinates[3, 1] = 0
-coordinates[3, 2] = 1
-coordinates[4, 1] = 1
-coordinates[4, 2] = 1
-coordinates[5, 1] = 2
-coordinates[5, 2] = 2
+    coordinates = zeros(Float64, 5, 2)
+    coordinates[1, 1] = 0
+    coordinates[1, 2] = 0
+    coordinates[2, 1] = 1
+    coordinates[2, 2] = 0
+    coordinates[3, 1] = 0
+    coordinates[3, 2] = 1
+    coordinates[4, 1] = 1
+    coordinates[4, 2] = 1
+    coordinates[5, 1] = 2
+    coordinates[5, 2] = 2
 
-nlist = [2, 3, 4, 5]
+    nlist = [2, 3, 4, 5]
 
+    bond_horizon::Float64 = 1
 
-bond_horizon::Float64 = 1
+    @test find_local_neighbors(coordinates[5,:], coordinates[nlist[nlist.!=5],:], nlist[nlist.!=5], bond_horizon) == []
+    bond_horizon = 2.6
+    @test find_local_neighbors(coordinates[5,:], coordinates[nlist[nlist.!=5],:], nlist[nlist.!=5], bond_horizon) == [2, 3, 4]
+    @test find_local_neighbors(coordinates[2,:], coordinates[nlist[nlist.!=2],:], nlist[nlist.!=2], bond_horizon) == [3, 4, 5]
+end
 
-@test find_local_neighbors(coordinates[5,:], coordinates[nlist[nlist.!=5],:], nlist[nlist.!=5], bond_horizon) == []
-bond_horizon::Float64 = 2.6
-@test find_local_neighbors(coordinates[5,:], coordinates[nlist[nlist.!=5],:], nlist[nlist.!=5], bond_horizon) == [2, 3, 4]
-@test find_local_neighbors(coordinates[2,:], coordinates[nlist[nlist.!=2],:], nlist[nlist.!=2], bond_horizon) == [3, 4, 5]
+@testset "ut_compute_bond_associated_weighted_volume"
+    # Initialize input arrays
+    nodes = [1, 2, 3]
+    nlist = [ [2, 3], [1, 3], [1, 2] ]
+    coordinates = [ 0.0 0.0; 1.0 0.0; 0.0 1.0 ]
+    bond_damage = [ [1.0, 1.0], [0.0, 1.0], [1.0, 1.0] ]
+    omega = [ [0.5, 0.8], [0.7, 0.9], [0.6, 0.4] ]
+    volume = [1.0, 2.0, 3.0]
+    weighted_volume =  [ [0.0, 0.0], [0.0, 0.0], [0.0, 0.0] ]
+    bond_horizon = 2.2
+    # Call the function under test
+    weighted_volume = compute_bond_associated_weighted_volume(nodes, nlist, coordinates, bond_damage, omega, volume, bond_horizon, weighted_volume)
+    # Check the expected output
+    @test weighted_volume ≈ [[0.7058823529411765, 0.2941176470588235], [1.0, 0.0], [0.5714285714285715, 0.4285714285714286]]
 end
