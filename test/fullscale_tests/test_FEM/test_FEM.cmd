e80856c77559dd0c2284127affd4d997e462272d
Jan-Timo Hesse <jan-timo.hesse@dlr.de>
2024-05-17 15:19:04 +0200

    Fixed FEM

--- test/fullscale_tests/test_FEM/test_FEM.cmd
+++ test/fullscale_tests/test_FEM/test_FEM.cmd
@@ -2,3 +2,6 @@ DEFAULT TOLERANCE absolute 1.0E-9
 COORDINATES absolute 1.0E-12
 TIME STEPS absolute 1.0E-14
 NODAL VARIABLES absolute 1.0E-12
+    "Cauchy Stressxy" absolute 1.0E-8
+    "Cauchy Stressyx" absolute 1.0E-8
+    "Cauchy Stressyy" absolute 1.0E-8
