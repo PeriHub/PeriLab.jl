c72e36f439c2f31e911f9c477f545ccd89d4385d
will_cr <christian.willberg@gmx.de>
2024-07-10 13:32:02 +0200

    new tests

--- test/fullscale_tests/test_correspondence_elastic/test_correspondence_elastic.jl
+++ test/fullscale_tests/test_correspondence_elastic/test_correspondence_elastic.jl
@@ -6,9 +6,10 @@
 
 folder_name = basename(@__FILE__)[1:end-3]
 cd("fullscale_tests/" * folder_name) do
-    # run_perilab("correspondence_strain_pstrain_xx", 1, true, folder_name)
+    run_perilab("correspondence_strain_pstrain_xx", 1, true, folder_name)
     run_perilab("correspondence_strain_pstrain_xy", 1, true, folder_name)
     # run_perilab("correspondence_strain_pstrain_yy", 1, true, folder_name)
+    run_perilab("ba_correspondence_strain_pstress_xx", 1, true, folder_name)
     # run_perilab("correspondence_strain_pstress_xx", 1, true, folder_name)
     run_perilab("correspondence_strain_pstress_xy", 1, true, folder_name)
     # run_perilab("correspondence_strain_pstress_yy", 1, true, folder_name)
