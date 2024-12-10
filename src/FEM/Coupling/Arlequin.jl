6c196db49d080430395896abb3794e47c25f1b53
will_cr <christian.willberg@gmx.de>
2024-12-10 18:37:51 +0100

    first step with couling

--- src/FEM/Coupling/Arlequin.jl
+++ src/FEM/Coupling/Arlequin.jl
@@ -3,6 +3,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 module Arlequin_coupling
+include("../Element_formulation/lagrange_element.jl")
+using .Lagrange_element:
+    define_lagrangian_grid_space, get_recursive_lagrange_shape_functions
+using LinearAlgebra
 function coupling_name()
     return "Arlequin"
 end
@@ -11,7 +15,7 @@ function init_coupling_model(datamanager::Module, complete_params::Dict)
 
     datamanager.create_constant_node_field("Number of Element Neighbors", Int64, 1, 1)
     # Find number of element neighbors
-    datamanager.create_constant_element_field("Coupling Elementlist", Int64, 1)
+    datamanager.create_constant_element_field("Coupling Element List", Int64, 1)
     # Assign Element ids
 
     return datamanager
@@ -23,7 +27,7 @@ function compute_coupling(
     fem_params::Dict,
 )
 
-    coupling_elements = datamanager.get_field("Coupling Elementlist") # elements connected to PD nodes
+    coupling_elements = datamanager.get_field("Coupling Element List") # elements connected to PD nodes
     # p1->(E1,E4)->E1-> 1,3,7,2..
     topology = datamanager.get_field("FE Topology")
     coordinates = datamanager.get_field("Coordinates")
@@ -52,5 +56,319 @@ end
 
 
 
+
+function compute_coupling_matrix(
+    coordinates,
+    topology,
+    point_val,
+    element_number,
+    kappa,
+    p,
+    dof,
+)
+    # only one point per call
+    # Point coordinates
+    x_point = coordinates[point_val, 1]
+    y_point = coordinates[point_val, 2]
+    ### Right now only for linear elements
+    x1 = coordinates[topology[element_number, 1], 1]
+    y1 = coordinates[topology[element_number, 1], 2]
+    x2 = coordinates[topology[element_number, 2], 1]
+    y2 = coordinates[topology[element_number, 2], 2]
+    x3 = coordinates[topology[element_number, 3], 1]
+    y3 = coordinates[topology[element_number, 3], 2]
+    x4 = coordinates[topology[element_number, 4], 1]
+    y4 = coordinates[topology[element_number, 4], 2]
+    ###
+    dx = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+
+    # Compute diagonal lengths
+    diag1 = sqrt((x3 - x1)^2 + (y3 - y1)^2)
+    diag2 = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+    diag3 = sqrt((x4 - x1)^2 + (y4 - y1)^2)
+
+    if diag1 > dx
+        ksi = 2 * (x_point - (x3 + x1) / 2) / dx
+        eta = 2 * (y_point - (y3 + y1) / 2) / dx
+    elseif diag2 > dx
+        ksi = 2 * (x_point - (x2 + x1) / 2) / dx
+        eta = 2 * (y_point - (y2 + y1) / 2) / dx
+    elseif diag3 > dx
+        ksi = 2 * (x_point - (x4 + x1) / 2) / dx
+        eta = 2 * (y_point - (y4 + y1) / 2) / dx
+    else
+        # Fallback to center or default values
+        ksi = 0.0
+        eta = 0.0
+    end
+    xi = define_lagrangian_grid_space(dof, p)
+    Nxi = get_recursive_lagrange_shape_functions(xi[1, :], ksi, p[1])
+    Neta = get_recursive_lagrange_shape_functions(xi[2, :], eta, p[2])
+    # shape functions of PD point in local coord
+    N1p = 0.25 * Nxi[1] * Neta[1] #(1 - ksi) * (1 - eta)
+    N2p = 0.25 * Nxi[2] * Neta[1] #(1 + ksi) * (1 - eta)
+    N3p = 0.25 * Nxi[2] * Neta[2] #(1 + ksi) * (1 + eta)
+    N4p = 0.25 * Nxi[1] * Neta[2] #(1 - ksi) * (1 + eta)
+
+    I = ones(1, 1)
+    Np = [N1p N2p N3p N4p]
+
+    local_coupl_matrix = kappa * [I -Np; -Np' Np'*Np]
+
+    return local_coupl_matrix
+end
+
+
+function find_point_in_elements(coordinates, topology, points_to_check)
+
+    # Will store which element each point is in ->
+    point_locations = Dict{Int64,Int64}()
+
+    # Iterate through points to check
+    @views for (point_index, point_coords) in
+               enumerate(eachrow(coordinates[points_to_check, :]))
+        # Check each element
+        for (element_index, element_vertices) in enumerate(eachrow(topology))
+            # Get vertex coordinates for this element
+            poly_vertices = [coordinates[v, :] for v in element_vertices]
+
+            # Check if point is in this element
+            if is_point_in_polygon(point_coords, poly_vertices)
+                point_locations[points_to_check[point_index]] = element_index
+                break
+            end
+        end
+    end
+
+    return point_locations
+end
+
+
+function is_point_in_polygon(point, polygon_vertices)
+    # Point-in-polygon test using ray casting method
+    x, y = point
+    n = length(polygon_vertices)
+    inside = false
+
+    j = n
+    for i = 1:n
+        @views xi, yi = polygon_vertices[i]
+        @views xj, yj = polygon_vertices[j]
+
+        intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+
+        if intersect
+            inside = !inside
+        end
+
+        j = i
+    end
+
+    return inside
+end
+#=
+
+# Imput section
+dof = 2
+elements = 1:2  # FE        #@info elements
+nodes = 1:10   # beide
+nFEnodes = 6            # number of FE nodes
+nodesFE = nodes[1:nFEnodes, 1]
+nodesPD = nodes[nFEnodes+1:end]
+topology = Array{Int64}(zeros(2, 4))
+topology[1, :] = [1, 2, 4, 6]
+topology[2, :] = [6, 4, 3, 5]
+
+coordinates = zeros(11, 2)
+coordinates[1, :] = [2, 0]
+coordinates[2, :] = [2, 1]      #FE
+coordinates[3, :] = [0, 1]
+coordinates[4, :] = [1, 1]      #FE
+coordinates[5, :] = [0, 0]
+coordinates[6, :] = [1, 0]      #FE
+coordinates[7, :] = [0.5, 0.5]
+coordinates[8, :] = [1.5, 0.5]     #PD
+coordinates[9, :] = [0.5, 1.5]
+coordinates[10, :] = [1.5, 1.5]    #PD
+
+# Define the coupling region; also as an imput nodes and elements in coupling region should be given
+coupling_elements = [1; 2]       #FE should be defined in input
+coupling_nodes = [7; 8]          #PD should be defined in input
+
+# find FE nodes, corresponds to coupling_elements
+FE_nodes = Int[]
+for i in coupling_elements
+    FE_nodes = [FE_nodes; topology[i, :]]
+end
+coupling_FE_nodes = unique(FE_nodes)
+
+
+kappa = 1
+coupl_matrix = Array{Float64}(zeros(5, 5, length(coupling_nodes)))
+weight_coefficient = 0.5 # just here, should be changed in future to a function
+
+using LinearAlgebra
+
+function is_point_in_polygon(point, polygon_vertices)
+    # Point-in-polygon test using ray casting method
+    x, y = point
+    n = length(polygon_vertices)
+    inside = false
+
+    j = n
+    for i in 1:n
+        xi, yi = polygon_vertices[i]
+        xj, yj = polygon_vertices[j]
+
+        intersect = ((yi > y) != (yj > y)) &&
+                    (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+
+        if intersect
+            inside = !inside
+        end
+
+        j = i
+    end
+
+    return inside
+end
+
+function find_point_in_elements(coordinates, topology, points_to_check)
+    num_elements = size(topology, 1)   # Number of elements
+
+    # Will store which element each point is in
+    point_locations = zeros(Int, length(points_to_check))
+
+    # Iterate through points to check
+    for (point_index, point_coords) in enumerate(eachrow(coordinates[points_to_check, :]))
+        # Check each element
+        for (element_index, element_vertices) in enumerate(eachrow(topology))
+            # Get vertex coordinates for this element
+            poly_vertices = [coordinates[v, :] for v in element_vertices]
+
+            # Check if point is in this element
+            if is_point_in_polygon(point_coords, poly_vertices)
+                point_locations[point_index] = element_index
+                break
+            end
+        end
+    end
+
+    return point_locations
+end
+
+
+# Building coupling matrix
+
+function compute_coupl_matrix(coordinates, topology, point_val, element_number, kappa)
+    # Point coordinates
+    x_point = coordinates[point_val, 1]
+    y_point = coordinates[point_val, 2]
+
+    x1 = coordinates[topology[element_number, 1], 1]
+    y1 = coordinates[topology[element_number, 1], 2]
+    x2 = coordinates[topology[element_number, 2], 1]
+    y2 = coordinates[topology[element_number, 2], 2]
+    x3 = coordinates[topology[element_number, 3], 1]
+    y3 = coordinates[topology[element_number, 3], 2]
+    x4 = coordinates[topology[element_number, 4], 1]
+    y4 = coordinates[topology[element_number, 4], 2]
+
+    dx = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+
+    # Compute diagonal lengths
+    diag1 = sqrt((x3 - x1)^2 + (y3 - y1)^2)
+    diag2 = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+    diag3 = sqrt((x4 - x1)^2 + (y4 - y1)^2)
+
+    if diag1 > dx
+        ksi = 2 * (x_point - (x3 + x1) / 2) / dx
+        eta = 2 * (y_point - (y3 + y1) / 2) / dx
+    elseif diag2 > dx
+        ksi = 2 * (x_point - (x2 + x1) / 2) / dx
+        eta = 2 * (y_point - (y2 + y1) / 2) / dx
+    elseif diag3 > dx
+        ksi = 2 * (x_point - (x4 + x1) / 2) / dx
+        eta = 2 * (y_point - (y4 + y1) / 2) / dx
+    else
+        # Fallback to center or default values
+        ksi = 0.0
+        eta = 0.0
+    end
+    # shape functions of PD point in local coord
+    N1p = 0.25 * (1 - ksi) * (1 - eta)
+    N2p = 0.25 * (1 + ksi) * (1 - eta)
+    N3p = 0.25 * (1 + ksi) * (1 + eta)
+    N4p = 0.25 * (1 - ksi) * (1 + eta)
+
+    I = ones(1, 1)
+    Np = [N1p N2p N3p N4p]
+
+    local_coupl_matrix = kappa * [I -Np; -Np' Np'*Np]
+
+    return local_coupl_matrix
+end
+
+
+points_to_check = coupling_nodes
+point_locations = find_point_in_elements(coordinates, topology, points_to_check)        #number of elements corresponds to PD overlapping nodes
+
+
+for (point_idx, point_val) in enumerate(coupling_nodes)
+    element_number = point_locations[point_idx]
+    local_coupl_matrix = compute_coupl_matrix(coordinates, topology, point_val, element_number, kappa)
+    coupl_matrix[:, :, point_idx] = local_coupl_matrix
+end
+
+stiffmatrFE = [0.1215 -0.0391 0.0087 -0.0130 0 0 -0.0608 0.0391 0 0 -0.0694 0.0130;
+    -0.0391 0.1215 0.0130 -0.0694 0 0 0.0391 -0.0608 0 0 -0.0130 0.0087;
+    0.0087 0.0130 0.1215 0.0391 0 0 -0.0694 -0.0130 0 0 -0.0608 -0.0391;
+    -0.0130 -0.0694 0.0391 0.1215 0 0 0.0130 0.0087 0 0 -0.0391 -0.0608;
+    0 0 0 0 0.1215 -0.0391 -0.0694 0.0130 0.0087 -0.0130 -0.0608 0.0391;
+    0 0 0 0 -0.0391 0.1215 -0.0130 0.0087 0.0130 -0.0694 0.0391 -0.0608;
+    -0.0608 0.0391 -0.0694 0.0130 -0.0694 -0.0130 0.2431 -0.0000 -0.0608 -0.0391 0.0174 0;
+    0.0391 -0.0608 -0.0130 0.0087 0.0130 0.0087 -0.0000 0.2431 -0.0391 -0.0608 0 -0.1389;
+    0 0 0 0 0.0087 0.0130 -0.0608 -0.0391 0.1215 0.0391 -0.0694 -0.0130;
+    0 0 0 0 -0.0130 -0.0694 -0.0391 -0.0608 0.0391 0.1215 0.0130 0.0087;
+    -0.0694 -0.0130 -0.0608 -0.0391 -0.0608 0.0391 0.0174 0 -0.0694 0.0130 0.2431 -0.0000;
+    0.0130 0.0087 -0.0391 -0.0608 0.0391 -0.0608 0 -0.1389 -0.0130 0.0087 -0.0000 0.2431]
+
+
+stiffPD = 0.01     # some factor just for simple case
+
+# Time integration Solver
+
+displacement = Array{Float64}(zeros(length(coordinates)))   # depends what structure has this vector in original code, here it is (x1,y1,x2,y2... )'
+
+#displacement = fill(0.1, 10, dof)
+
+for tt in 1:100
+
+    force_internal_FE = stiffmatrFE * displacement[nodesFE*dof]
+    force_internal_PD = force_dencities #here both x and Y coord
+    force_internal = vcat(force_internal_FE, force_internal_PD)
+    # calculation of damage
+
+    # Add weightening
+    for ipd in coupling_nodes
+        force_internal[ipd*dof-1:ipd*dof] *= weight_coefficient
+    end
+
+    for ife in coupling_FE_nodes
+        force_internal[ife*dof-1:ife*dof] *= weight_coefficient
+    end
+
+    # Add weightening for mass vector same way
+
+    # Add to the system coupling matrix
+    for (point_idx, point_val) in enumerate(coupling_nodes)
+        topo_local = [point_val; topology[point_locations[point_idx], :]] # global numbering of coupled nodes [xpd x1fe x2fe x3fe x4fe]
+        ...
+        # depends how vectors are constructed
+    end
+end
+
+
+=#
 
 end
--- test/unit_tests/FEM/Coupling/ut_Arlequin.jl
+++ test/unit_tests/FEM/Coupling/ut_Arlequin.jl
@@ -9,5 +9,77 @@ include("../../../../src/FEM/Coupling/Arlequin.jl")
 @testset "ut_coupling_name" begin
     @test Arlequin.coupling_name() == "Arlequin"
 end
+@testset "ut_find_point_in_elements" begin
+    dof = 2
+    elements = 1:2  # FE        #@info elements
+    nodes = 1:10   # beide
+    nFEnodes = 6            # number of FE nodes
+    nodesFE = nodes[1:nFEnodes, 1]
+    nodesPD = nodes[nFEnodes+1:end, 1]
+    topology = Array{Int64}(zeros(2, 4))
+    topology[1, :] = [1, 2, 4, 6]
+    topology[2, :] = [6, 4, 3, 5]
 
-Arlequin.find_point_in_elements(coordinates, topology, points_to_check)
+    coordinates = zeros(11, 2)
+    coordinates[1, :] = [2, 0]
+    coordinates[2, :] = [2, 1]      #FE
+    coordinates[3, :] = [0, 1]
+    coordinates[4, :] = [1, 1]      #FE
+    coordinates[5, :] = [0, 0]
+    coordinates[6, :] = [1, 0]      #FE
+    coordinates[7, :] = [0.5, 0.5]
+    coordinates[8, :] = [1.5, 0.5]     #PD
+    coordinates[9, :] = [0.5, 1.5]
+    coordinates[10, :] = [1.5, 1.5]    #PD
+
+    test_dict = Arlequin.find_point_in_elements(coordinates, topology, nodesPD)
+    @test collect(keys(test_dict)) == [7, 8]
+    @test test_dict[7] == 2
+    @test test_dict[8] == 1
+end
+
+@testset "ut_find_point_in_elements" begin
+    dof = 2
+    elements = 1:2  # FE        #@info elements
+    nodes = 1:10   # beide
+    nFEnodes = 6            # number of FE nodes
+    nodesFE = nodes[1:nFEnodes, 1]
+    nodesPD = nodes[nFEnodes+1:end, 1]
+    topology = Array{Int64}(zeros(2, 4))
+    topology[1, :] = [1, 2, 4, 6]
+    topology[2, :] = [6, 4, 3, 5]
+
+    coordinates = zeros(11, 2)
+    coordinates[1, :] = [2, 0]
+    coordinates[2, :] = [2, 1]      #FE
+    coordinates[3, :] = [0, 1]
+    coordinates[4, :] = [1, 1]      #FE
+    coordinates[5, :] = [0, 0]
+    coordinates[6, :] = [1, 0]      #FE
+    coordinates[7, :] = [0.5, 0.5]
+    coordinates[8, :] = [1.5, 0.5]     #PD
+    coordinates[9, :] = [0.5, 1.5]
+    coordinates[10, :] = [1.5, 1.5]    #PD
+    kappa = 1
+
+    p = [1, 1]
+    dof = 2
+
+    test_mat = compute_coupling_matrix(coordinates, topology, 7, 2, kappa, p, dof)
+    @test test_mat == [
+        1.0 -0.0625 -0.0625 -0.0625 -0.0625
+        -0.0625 0.00390625 0.00390625 0.00390625 0.00390625
+        -0.0625 0.00390625 0.00390625 0.00390625 0.00390625
+        -0.0625 0.00390625 0.00390625 0.00390625 0.00390625
+        -0.0625 0.00390625 0.00390625 0.00390625 0.00390625
+    ]
+    test_mat = compute_coupling_matrix(coordinates, topology, 8, 1, kappa, p, dof)
+
+    @test test_mat == [
+        1.0 -0.0625 -0.0625 -0.0625 -0.0625
+        -0.0625 0.00390625 0.00390625 0.00390625 0.00390625
+        -0.0625 0.00390625 0.00390625 0.00390625 0.00390625
+        -0.0625 0.00390625 0.00390625 0.00390625 0.00390625
+        -0.0625 0.00390625 0.00390625 0.00390625 0.00390625
+    ]
+end
