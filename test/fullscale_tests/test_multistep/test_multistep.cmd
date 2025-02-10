7f774b795067750e54bc897d8d4e1d6405f8dc50
hess_ja <jan-timo.hesse@dlr.de>
2025-02-10 18:13:24 +0100

    fix test

--- test/fullscale_tests/test_multistep/test_multistep.cmd
+++ test/fullscale_tests/test_multistep/test_multistep.cmd
@@ -3,5 +3,3 @@ COORDINATES absolute 1.0E-12
 TIME STEPS absolute 1.0E-14
 NODAL VARIABLES absolute 1.0E-12
 	Temperature   absolute 1.0E-8
-	Displacements absolute 1.0E-8
-	Forces        absolute 1.0E-8
