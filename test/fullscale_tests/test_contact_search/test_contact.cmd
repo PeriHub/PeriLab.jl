21ad8ae9142d3953a8e52494c3e43aa1b72146a6
will_cr <christian.willberg@gmx.de>
2025-04-01 13:27:47 +0200

    prototype full scale test

---
+++ test/fullscale_tests/test_contact_search/test_contact.cmd
@@ -0,0 +1,7 @@
+DEFAULT TOLERANCE absolute 1.0E-9
+COORDINATES absolute 1.0E-12
+TIME STEPS absolute 1.0E-14
+NODAL VARIABLES absolute 1.0E-12
+	DisplacementsX   absolute 1.0E-9
+	DisplacementsY   absolute 1.0E-9
+	DisplacementsZ   absolute 1.0E-9
