eb87008efe3dac6306e32b4c12191b54e2f4d9b5
hess_ja <jan-timo.hesse@dlr.de>
2025-04-29 12:59:33 +0200

    fix test

--- test/fullscale_tests/test_FEM_coupling/test_FEM_coupling.cmd
+++ test/fullscale_tests/test_FEM_coupling/test_FEM_coupling.cmd
@@ -4,7 +4,7 @@ TIME STEPS absolute 1.0E-14
 NODAL VARIABLES absolute 1.0E-12
     DisplacementsX    absolute 1.0E-8
     DisplacementsY    absolute 1.0E-8
-    "Cauchy Stressyy" absolute 1.0E-5
-    "Cauchy Stressxy" absolute 1.0E-5
-    "Cauchy Stressyx" absolute 1.0E-5
-    "Cauchy Stressyy" absolute 1.0E-5
+    "Cauchy Stressyy" absolute 1.0E-4
+    "Cauchy Stressxy" absolute 1.0E-4
+    "Cauchy Stressyx" absolute 1.0E-4
+    "Cauchy Stressyy" absolute 1.0E-4
