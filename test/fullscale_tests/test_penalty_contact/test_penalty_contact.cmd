502dfe3326516fd58d2ce15c89fabfe6c7bdc5ea
hess_ja <jan-timo.hesse@dlr.de>
2025-05-05 19:31:11 +0200

    refac

--- test/fullscale_tests/test_penalty_contact/test_penalty_contact.cmd
+++ test/fullscale_tests/test_penalty_contact/test_penalty_contact.cmd
@@ -5,6 +5,6 @@ NODAL VARIABLES absolute 1.0E-12
 	DisplacementsX   absolute 1.0E-9
 	DisplacementsY   absolute 1.0E-9
 	ForcesX   absolute 1.0E-9
-	DForcessY   absolute 1.0E-9
+	ForcesY   absolute 1.0E-9
 	VelocityX   absolute 1.0E-9
 	VelocityY   absolute 1.0E-9
