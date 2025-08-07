e9ba272eafab39feddf6d08a0089cacca9eae154
hess_ja <jan-timo.hesse@dlr.de>
2025-08-07 09:11:55 +0200

    refac

--- test/unit_tests/Models/Thermal/ut_Heat_transfer.jl
+++ test/unit_tests/Models/Thermal/ut_Heat_transfer.jl
@@ -69,16 +69,16 @@ using .Heat_transfer
                                              specific_volume_check,
                                              dof)
     @test specific_volume == [
-        2.0,
-        1.0,
-        2.0,
-        1.0,
-        0.0,
-        1.0,
-        2.0,
-        1.0,
-        2.0,
-        3.0
+        2,
+        1,
+        3,
+        1,
+        0,
+        1,
+        2,
+        1,
+        2,
+        3
     ]
 end
 
