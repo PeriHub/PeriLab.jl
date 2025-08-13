7003901a78fe8420ccfb871cd1461c8b920354a8
hess_ja <jan-timo.hesse@dlr.de>
2025-08-13 10:02:08 +0200

    refac

--- test/fullscale_tests/test_thermal_flow/test_thermal_flow.cmd
+++ test/fullscale_tests/test_thermal_flow/test_thermal_flow.cmd
@@ -3,4 +3,4 @@ COORDINATES absolute 1.0E-12
 TIME STEPS absolute 1.0E-14
 NODAL VARIABLES absolute 1.0E-12
 	Temperature   absolute 1.0E-8
-	"Heat Flow"   absolute 1.0E-5
+	"Heat Flow"   absolute 1.0E-8
