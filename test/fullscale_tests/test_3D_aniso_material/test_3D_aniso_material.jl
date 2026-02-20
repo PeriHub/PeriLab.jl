b5b4e6aecbc01b3192fc8771cebdfd6304be78f3
will_cr <christian.willberg@gmx.de>
2026-02-20 11:17:32 +0100

    solve #312

---
+++ test/fullscale_tests/test_3D_aniso_material/test_3D_aniso_material.jl
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Christian Willberg <christian.willberg@dlr.de>, Jan-Timo Hesse <jan-timo.hesse@dlr.de>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+folder_name = basename(@__FILE__)[1:(end - 3)]
+cd("fullscale_tests/" * folder_name) do
+    run_perilab("3D_anisomat", 1, true, folder_name)
+end
