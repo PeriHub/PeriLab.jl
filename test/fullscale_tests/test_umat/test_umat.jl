e65abc814c3b33bbcfb5032bb652574949eab441
hess_ja <jan-timo.hesse@dlr.de>
2026-05-18 13:53:15 +0200

    added umat test #355

--- test/fullscale_tests/test_umat/test_umat.jl
+++ test/fullscale_tests/test_umat/test_umat.jl
@@ -4,5 +4,7 @@
 
 folder_name = basename(@__FILE__)[1:(end - 3)]
 cd("fullscale_tests/" * folder_name) do
+    run_perilab("umat", 1, false, folder_name)
     run_perilab("vumat", 1, false, folder_name)
+    run_perilab("umat_matrix", 1, false, folder_name)
 end
