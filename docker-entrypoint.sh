a1d10b3f4b11aa6d9080700e3781b52fa34708b8
hess_ja <jan-timo.hesse@dlr.de>
2026-09-22 10:46:01 +0200

    fix image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,4 @@
-# SPDX-FileCopyrightText: 2023 Christian Willberg <christian.willberg@dlr.de>, Jan-Timo Hesse <jan-timo.hesse@dlr.de>
-#
-# SPDX-License-Identifier: BSD-3-Clause
+#!/bin/bash
 set -e
 
 rm -f ~/.julia/compiled/v1.12/PeriLab/*.ji
