74173851e211bfda5d26c01a71fe0a83059296d2
hess_ja <jan-timo.hesse@dlr.de>
2026-09-25 15:14:05 +0200

    refac

--- test/fullscale_tests/test_3D_aniso_material/test_3D_aniso_material.cmd
+++ test/fullscale_tests/test_3D_aniso_material/test_3D_aniso_material.cmd
@@ -4,6 +4,6 @@ TIME STEPS absolute 1.0E-14
 NODAL VARIABLES absolute 1.0E-12
 	DisplacementsX   absolute 1.0E-8
 	DisplacementsY   absolute 1.0E-8
-	ForcesX          absolute 1.0E-6
-	ForcesY          absolute 1.0E-6
+	ForcesX          absolute 1.0E-5
+	ForcesY          absolute 1.0E-5
 	"Number of Neighbors" absolute 1.0E-7
